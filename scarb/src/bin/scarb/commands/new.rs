b59713a613b6ba64f14fe5c4a89112a393622ba5
Marek Kaput <marek.kaput@swmansion.com>
2023-01-16 12:06:17 +0100

    Implement `scarb new` and `scarb init` commands

---
+++ scarb/src/bin/scarb/commands/new.rs
@@ -0,0 +1,20 @@
+use anyhow::Result;
+
+use scarb::core::Config;
+use scarb::ops;
+
+use crate::args::NewArgs;
+
+#[tracing::instrument(skip_all, level = "info")]
+pub fn run(args: NewArgs, config: &Config) -> Result<()> {
+    let result = ops::new_package(
+        ops::InitOptions {
+            name: args.init.name,
+            path: args.path,
+        },
+        config,
+    )?;
+
+    println!("Created `{}` package.", result.name);
+    Ok(())
+}
---
+++ scarb/src/ops/new.rs
@@ -0,0 +1,156 @@
+use std::path::{Path, PathBuf};
+
+use anyhow::{bail, ensure, Context, Result};
+use indoc::{formatdoc, indoc};
+
+use crate::core::{restricted_names, Config, PackageName};
+use crate::internal::fsx;
+use crate::{DEFAULT_SOURCE_DIR_NAME, DEFAULT_TARGET_DIR_NAME, MANIFEST_FILE_NAME};
+
+#[derive(Debug)]
+pub struct InitOptions {
+    pub path: PathBuf,
+    pub name: Option<PackageName>,
+}
+
+#[derive(Debug)]
+pub struct NewResult {
+    pub name: PackageName,
+}
+
+pub fn new_package(opts: InitOptions, config: &Config) -> Result<NewResult> {
+    ensure!(
+        !opts.path.exists(),
+        "destination `{}` already exists\nUse `scarb init` to initialize the directory.",
+        opts.path.display()
+    );
+
+    let name = infer_name(opts.name, &opts.path)?;
+    check_name(&name)?;
+
+    mk(
+        MkOpts {
+            path: opts.path.clone(),
+            name: name.clone(),
+        },
+        config,
+    )
+    .with_context(|| {
+        format!(
+            "failed to create package `{name}` at `{}`",
+            opts.path.display()
+        )
+    })?;
+
+    Ok(NewResult { name })
+}
+
+pub fn init_package(opts: InitOptions, config: &Config) -> Result<NewResult> {
+    ensure!(
+        !opts.path.join(MANIFEST_FILE_NAME).exists(),
+        "`scarb init` cannot be run on existing Scarb packages"
+    );
+
+    let name = infer_name(opts.name, &opts.path)?;
+    check_name(&name)?;
+
+    mk(
+        MkOpts {
+            path: opts.path,
+            name: name.clone(),
+        },
+        config,
+    )
+    .with_context(|| format!("failed to create package `{name}`",))?;
+
+    Ok(NewResult { name })
+}
+
+fn infer_name(name: Option<PackageName>, path: &Path) -> Result<PackageName> {
+    if let Some(name) = name {
+        Ok(name)
+    } else {
+        let Some(file_name) = path.file_name() else {
+            bail!(
+                "cannot infer package name from path {:?}\nUse --name to override.",
+                path.as_os_str()
+            );
+        };
+
+        let Some(file_name) = file_name.to_str() else {
+            bail!("cannot create package with a non-unicode name: {:?}", file_name);
+        };
+
+        Ok(file_name.into())
+    }
+}
+
+fn check_name(name: &PackageName) -> Result<()> {
+    restricted_names::validate_package_name(name, "package name")?;
+    // TODO(mkaput): Check not Cairo keyword.
+    // TODO(mkaput): Warn if name is `corelib`.
+    Ok(())
+}
+
+struct MkOpts {
+    path: PathBuf,
+    name: PackageName,
+}
+
+fn mk(MkOpts { path, name }: MkOpts, _config: &Config) -> Result<()> {
+    // Create project directory in case we are called from `new` op.
+    fsx::create_dir_all(&path)?;
+
+    // Write VCS ignore file.
+    // TODO(mkaput): Print a message to the user that they need to add `target` themself.
+    let gitignore = path.join(".gitignore");
+    if !gitignore.exists() {
+        fsx::write(gitignore, DEFAULT_TARGET_DIR_NAME)?;
+    }
+
+    // Create the `Scarb.toml` file.
+    fsx::write(
+        path.join(MANIFEST_FILE_NAME),
+        formatdoc! {r#"
+            [package]
+            name = "{name}"
+            version = "0.1.0"
+
+            # See more keys and their definitions at https://github.com/software-mansion/scarb/blob/main/scarb/src/core/manifest/toml.rs
+
+            [dependencies]
+            # foo = {{ path = "vendor/foo" }}
+        "#},
+    )?;
+
+    // Create hello world source files (with respective parent directories) if source directory
+    // does not exist.
+    let source_dir = path.join(DEFAULT_SOURCE_DIR_NAME);
+    if !source_dir.exists() {
+        fsx::create_dir_all(&source_dir)?;
+
+        fsx::write(
+            source_dir.join("lib.cairo"),
+            indoc! {r#"
+                fn fib(a: felt, b: felt, n: felt) -> felt {
+                    match n {
+                        0 => a,
+                        _ => fib(b, a + b, n - 1),
+                    }
+                }
+            "#},
+        )?;
+    }
+
+    // TODO(mkaput): Take this from Cargo:
+    //   if let Err(e) = Workspace::new(&path.join("Cargo.toml"), config) {
+    //       crate::display_warning_with_error(
+    //           "compiling this new package may not work due to invalid \
+    //            workspace configuration",
+    //           &e,
+    //           &mut config.shell(),
+    //       );
+    //   }
+
+    Ok(())
+}
