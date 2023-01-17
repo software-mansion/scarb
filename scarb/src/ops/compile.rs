9df5cc39b5f8a5ebd741cb476b3cad45796433fa
Marek Kaput <marek.kaput@swmansion.com>
2023-01-17 11:25:28 +0100

    Trace log `ProjectConfig` in `ops::compile`

--- scarb/src/ops/compile.rs
+++ scarb/src/ops/compile.rs
@@ -2,7 +2,7 @@ use std::fs;
 
 use anyhow::Result;
 use cairo_lang_compiler::project::{ProjectConfig, ProjectConfigContent};
-use cairo_lang_compiler::CompilerConfig;
+use cairo_lang_compiler::{CompilerConfig, SierraProgram};
 
 use crate::core::workspace::Workspace;
 use crate::core::PackageId;
@@ -16,13 +16,7 @@ pub fn compile(ws: &Workspace<'_>, on_diagnostic: Box<dyn FnMut(String)>) -> Res
     let resolve = ops::resolve_workspace(ws)?;
     let project_config = build_project_config(current_package.id, &resolve)?;
 
-    let sierra_program = cairo_lang_compiler::compile(
-        project_config,
-        CompilerConfig {
-            on_diagnostic: Some(on_diagnostic),
-            ..CompilerConfig::default()
-        },
-    )?;
+    let sierra_program = run_compile(project_config, on_diagnostic)?;
 
     fs::write(
         ws.target_dir()
@@ -52,4 +46,18 @@ fn build_project_config(member_id: PackageId, resolve: &WorkspaceResolve) -> Res
         base_path: resolve.packages[&member_id].root().into(),
         content,
     })
+}
+
+#[tracing::instrument(level = "trace", skip(on_diagnostic))]
+fn run_compile(
+    project_config: ProjectConfig,
+    on_diagnostic: Box<dyn FnMut(String)>,
+) -> Result<SierraProgram> {
+    cairo_lang_compiler::compile(
+        project_config,
+        CompilerConfig {
+            on_diagnostic: Some(on_diagnostic),
+            ..CompilerConfig::default()
+        },
+    )
 }
