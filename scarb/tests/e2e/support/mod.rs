e736519a64261577d4de1879cf78d0b266e1f091
Marek Kaput <marek.kaput@swmansion.com>
2023-02-10 14:44:52 +0100

    Add Git test support code

--- scarb/tests/e2e/support/mod.rs
+++ scarb/tests/e2e/support/mod.rs
@@ -1,3 +1,4 @@
 pub mod command;
 pub mod fsx;
+pub mod gitx;
 pub mod project_builder;
--- scarb/tests/e2e/support/project_builder.rs
+++ scarb/tests/e2e/support/project_builder.rs
@@ -1,10 +1,14 @@
 use std::sync::atomic::{AtomicU64, Ordering};
 
+use assert_fs::fixture::ChildPath;
 use assert_fs::prelude::*;
-use indoc::formatdoc;
 use semver::Version;
+use toml_edit::{Document, Item, Table, Value};
 
 use to_version::ToVersion;
+
+use crate::support::fsx::PathUtf8Ext;
+use crate::support::gitx::GitProject;
 
 #[path = "../../../src/internal/to_version.rs"]
 mod to_version;
@@ -13,6 +17,7 @@ pub struct ProjectBuilder {
     name: String,
     version: Version,
     lib_cairo: String,
+    deps: Vec<(String, Value)>,
 }
 
 impl ProjectBuilder {
@@ -25,6 +30,7 @@ impl ProjectBuilder {
             name: format!("pkg{n}"),
             version: Version::new(1, n, 0),
             lib_cairo: format!(r#"fn f{n}() -> felt {{ {n} }}"#),
+            deps: Vec::new(),
         }
     }
 
@@ -43,16 +49,22 @@ impl ProjectBuilder {
         self
     }
 
-    pub fn just_manifest(&self, t: &impl PathChild) {
-        let Self { name, version, .. } = self;
+    pub fn dep(mut self, name: impl Into<String>, dep: impl ToDep) -> Self {
+        self.deps.push((name.into(), dep.to_dep()));
+        self
+    }
 
-        t.child("Scarb.toml")
-            .write_str(&formatdoc! {r#"
-                [package]
-                name = "{name}"
-                version = "{version}"
-            "#})
-            .unwrap();
+    pub fn just_manifest(&self, t: &impl PathChild) {
+        let mut doc = Document::new();
+        doc["package"]["name"] = Item::Value(Value::from(self.name.clone()));
+        doc["package"]["version"] = Item::Value(Value::from(self.version.to_string()));
+
+        for (name, dep) in &self.deps {
+            doc["dependencies"][name.clone()] = Item::Value(dep.clone());
+        }
+
+        let manifest = doc.to_string();
+        t.child("Scarb.toml").write_str(&manifest).unwrap();
     }
 
     pub fn just_code(&self, t: &impl PathChild) {
@@ -63,5 +75,45 @@ impl ProjectBuilder {
         self.just_manifest(t);
 
         self.just_code(t);
+    }
+}
+
+pub trait ToDep {
+    fn to_dep(&self) -> Value;
+}
+
+impl ToDep for Table {
+    fn to_dep(&self) -> Value {
+        self.clone().into_inline_table().into()
+    }
+}
+
+impl ToDep for &str {
+    fn to_dep(&self) -> Value {
+        let doc = self.parse::<Document>().unwrap();
+        let tab = doc.as_table().clone().into_inline_table();
+        Value::InlineTable(tab)
+    }
+}
+
+impl ToDep for String {
+    fn to_dep(&self) -> Value {
+        self.as_str().to_dep()
+    }
+}
+
+impl ToDep for &ChildPath {
+    fn to_dep(&self) -> Value {
+        let mut table = toml_edit::table();
+        table["path"] = Item::Value(Value::from(self.path().try_to_utf8().unwrap().to_string()));
+        table.into_value().unwrap()
+    }
+}
+
+impl ToDep for &GitProject {
+    fn to_dep(&self) -> Value {
+        let mut table = toml_edit::table();
+        table["git"] = Item::Value(Value::from(self.url()));
+        table.into_value().unwrap()
     }
 }
