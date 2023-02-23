7b542f66c2b472caa9377da3a42e3c0c193dbe38
Maciej Trątnowiecki <maciej.tratnowiecki@swmansion.com>
2023-02-23 14:01:03 +0100

    Fix manifest env var

--- scarb/src/ops/subcommands.rs
+++ scarb/src/ops/subcommands.rs
@@ -27,7 +27,7 @@ pub fn execute_external_subcommand(cmd: &str, args: &[&OsStr], config: &Config) 
     cmd.env("SCARB_CACHE", config.dirs().cache_dir.path_unchecked());
     cmd.env("SCARB_CONFIG", config.dirs().config_dir.path_unchecked());
     cmd.env("SCARB_TARGET_DIR", config.target_dir().path_unchecked());
-    cmd.env("SCARB_MANIFEST_DIR", config.root());
+    cmd.env("SCARB_MANIFEST_PATH", config.manifest_path());
     cmd.env("SCARB_UI_VERBOSITY", config.ui().verbosity().to_string());
     cmd.env("SCARB_LOG", config.scarb_log());
     exec_replace(&mut cmd)
