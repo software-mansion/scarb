fed26067300a61fc0c7e10df3ed2b6f570ef4208
Marek Kaput <marek.kaput@swmansion.com>
2023-02-24 15:10:56 +0100

    Extract `compiler::helpers` module

--- scarb/src/compiler/targets/lib.rs
+++ scarb/src/compiler/targets/lib.rs
@@ -2,17 +2,14 @@ use std::io::Write;
 
 use anyhow::{Context, Result};
 use cairo_lang_compiler::db::RootDatabase;
-use cairo_lang_compiler::diagnostics::DiagnosticsReporter;
-use cairo_lang_compiler::project::{ProjectConfig, ProjectConfigContent};
-use cairo_lang_compiler::CompilerConfig;
-use cairo_lang_filesystem::db::FilesGroup;
-use cairo_lang_filesystem::ids::{CrateId, CrateLongId, Directory};
 use cairo_lang_sierra_to_casm::metadata::{calc_metadata, MetadataComputationConfig};
-use tracing::{trace, trace_span};
+use tracing::trace_span;
 
+use crate::compiler::helpers::{
+    build_compiler_config, build_project_config, collect_main_crate_ids,
+};
 use crate::compiler::{CompilationUnit, Compiler};
-use crate::core::{LibTargetKind, PackageName, Workspace};
-use crate::ui::TypedMessage;
+use crate::core::{LibTargetKind, Workspace};
 
 pub struct LibCompiler;
 
@@ -82,54 +79,4 @@ impl Compiler for LibCompiler {
 
         Ok(())
     }
-}
-
-pub(super) fn build_project_config(unit: &CompilationUnit) -> Result<ProjectConfig> {
-    let crate_roots = unit
-        .components
-        .iter()
-        .filter(|pkg| pkg.id.name != PackageName::CORE)
-        .map(|pkg| {
-            (
-                pkg.id.name.to_smol_str(),
-                pkg.source_dir().into_std_path_buf(),
-            )
-        })
-        .collect();
-
-    let corelib = unit
-        .components
-        .iter()
-        .find(|pkg| pkg.id.name == PackageName::CORE)
-        .map(|pkg| Directory(pkg.source_dir().into_std_path_buf()));
-
-    let content = ProjectConfigContent { crate_roots };
-
-    let project_config = ProjectConfig {
-        base_path: unit.package.root().into(),
-        corelib,
-        content,
-    };
-
-    trace!(?project_config);
-
-    Ok(project_config)
-}
-
-pub(super) fn build_compiler_config<'c>(ws: &Workspace<'c>) -> CompilerConfig<'c> {
-    CompilerConfig {
-        diagnostics_reporter: DiagnosticsReporter::callback({
-            let config = ws.config();
-            |diagnostic: String| {
-                config
-                    .ui()
-                    .print(TypedMessage::naked_text("diagnostic", &diagnostic));
-            }
-        }),
-        ..CompilerConfig::default()
-    }
-}
-
-pub(super) fn collect_main_crate_ids(unit: &CompilationUnit, db: &RootDatabase) -> Vec<CrateId> {
-    vec![db.intern_crate(CrateLongId(unit.package.id.name.to_smol_str()))]
 }
