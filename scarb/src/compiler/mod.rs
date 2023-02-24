c22a1802f9d2c80742606c5aad11be657e3a926c
Marek Kaput <marek.kaput@swmansion.com>
2023-02-24 14:42:39 +0100

    Rename `TargetCompiler` to `Compiler`

--- scarb/src/compiler/mod.rs
+++ scarb/src/compiler/mod.rs
@@ -1,6 +1,23 @@
+use anyhow::Result;
+
 pub use compilation_unit::*;
 pub use profile::*;
+
+use crate::core::Workspace;
 
 mod compilation_unit;
 mod profile;
 pub(crate) mod targets;
+
+pub trait Compiler {
+    fn compile(&self, unit: CompilationUnit, ws: &Workspace<'_>) -> Result<()>;
+}
+
+impl<F> Compiler for F
+where
+    F: Fn(CompilationUnit, &Workspace<'_>) -> Result<()>,
+{
+    fn compile(&self, unit: CompilationUnit, ws: &Workspace<'_>) -> Result<()> {
+        self(unit, ws)
+    }
+}
