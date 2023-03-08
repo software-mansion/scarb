e6c1854bfe5ec608667e4be8c3fd3c8eb7094c54
Marek Kaput <marek.kaput@swmansion.com>
2023-03-08 09:31:16 +0100

    Set up website deployment scripts

--- website/next.config.js
+++ website/next.config.js
@@ -1,7 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+/** @type {import("next").NextConfig} */
+let nextConfig = {
+  images: {
+    unoptimized: true,
+  },
   reactStrictMode: true,
 };
+
+const isProduction = process.env.NODE_ENV === "production";
+if (isProduction) {
+  const assetPrefix = "/scarb";
+  nextConfig = {
+    ...nextConfig,
+    assetPrefix,
+    basePath: assetPrefix,
+  };
+}
 
 const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
