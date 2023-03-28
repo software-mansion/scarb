289137c243159db8ab9c6d9b8a8e546c42074f66
Marek Kaput <marek.kaput@swmansion.com>
2023-03-28 08:30:29 +0200

    Prepare release `0.1.0`

--- website/theme.config.tsx
+++ website/theme.config.tsx
@@ -7,14 +7,14 @@ import { DocsThemeConfig } from "nextra-theme-docs";
 const config: DocsThemeConfig = {
   logo: <HeaderLogo />,
   primaryHue: { light: 239, dark: 204 },
-  // banner: {
-  //   key: "scarb-0.1-released",
-  //   text: (
-  //     <a href="https://github.com/software-mansion/scarb/releases/tag/v0.1.0">
-  //       🎉 Scarb v0.1 is released. Read more →
-  //     </a>
-  //   ),
-  // },
+  banner: {
+    key: "scarb-0.1-released",
+    text: (
+      <a href="https://github.com/software-mansion/scarb/releases/tag/v0.1.0">
+        🎉 Scarb v0.1 is released. Read more →
+      </a>
+    ),
+  },
   project: {
     link: "https://github.com/software-mansion/scarb",
   },
