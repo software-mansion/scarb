9e3c8d97dcd13d264e536a93a45255dac8b02a89
Marek Kaput <marek.kaput@swmansion.com>
2023-05-23 11:41:18 +0200

    Make `scarb clean` not error if target directory is not present
    commit-id:73e1fc5c

--- scarb/src/ops/clean.rs
+++ scarb/src/ops/clean.rs
@@ -6,5 +6,8 @@ use crate::internal::fsx;
 #[tracing::instrument(skip_all, level = "debug")]
 pub fn clean(config: &Config) -> Result<()> {
     let path = config.target_dir().path_unchecked();
-    fsx::remove_dir_all(path).context("failed to clean generated artifacts")
+    if path.exists() {
+        fsx::remove_dir_all(path).context("failed to clean generated artifacts")?;
+    }
+    Ok(())
 }
