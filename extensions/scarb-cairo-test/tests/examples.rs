c352a53ddaa6d4ba8ab18b7ae84b6bedb996b394
Marek Kaput <marek.kaput@swmansion.com>
2023-05-26 15:20:46 +0200

    Implement Scarb test runner

---
+++ extensions/scarb-cairo-test/tests/examples.rs
@@ -0,0 +1,14 @@
+use std::path::Path;
+
+use snapbox::cmd::{cargo_bin, Command};
+
+use test_for_each_example::test_for_each_example;
+
+#[test_for_each_example]
+fn cairo_test(example: &Path) {
+    Command::new(cargo_bin("scarb"))
+        .arg("cairo-test")
+        .current_dir(example)
+        .assert()
+        .success();
+}
