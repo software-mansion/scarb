09b825f8c2495105e2a0e2cff598f0a5189db0c9
maciektr <maciej.tratnowiecki@swmansion.com>
2023-06-20 12:50:15 +0200

    Add check_proc get_bitness

--- install.sh
+++ install.sh
@@ -107,6 +107,35 @@ main() {
   return "$_retval"
 }
 
+# This function has been copied verbatim from rustup install script.
+check_proc() {
+    # Check for /proc by looking for the /proc/self/exe link
+    # This is only run on Linux
+    if ! test -L /proc/self/exe ; then
+        err "fatal: Unable to find /proc/self/exe.  Is /proc mounted?  Installation cannot proceed without /proc."
+    fi
+}
+
+# This function has been copied verbatim from rustup install script.
+get_bitness() {
+    need_cmd head
+    # Architecture detection without dependencies beyond coreutils.
+    # ELF files start out "\x7fELF", and the following byte is
+    #   0x01 for 32-bit and
+    #   0x02 for 64-bit.
+    # The printf builtin on some shells like dash only supports octal
+    # escape sequences, so we use those.
+    local _current_exe_head
+    _current_exe_head=$(head -c 5 /proc/self/exe )
+    if [ "$_current_exe_head" = "$(printf '\177ELF\001')" ]; then
+        echo 32
+    elif [ "$_current_exe_head" = "$(printf '\177ELF\002')" ]; then
+        echo 64
+    else
+        err "unknown platform bitness"
+    fi
+}
+
 say() {
   printf 'scarb-install: %s\n' "$1"
 }
