70db5374d5a6c876036170644dbdb81909173365
Marek Kaput <marek.kaput@swmansion.com>
2023-08-16 11:28:37 +0200

    New init template
    This new template uses newer Cairo features (loops), and additionally is both runnable and testable.

--- examples/hello_world/README.md
+++ examples/hello_world/README.md
@@ -1,3 +1,16 @@
 # `hello_world`
 
 This example is a result of running `scarb new --no-vcs hello_world`.
+This template can be both run and tested, try running following commands:
+
+```shell
+scarb build
+```
+
+```shell
+scarb test
+```
+
+```shell
+scarb cairo-run --available-gas 200000
+```
--- scarb/src/ops/new.rs
+++ scarb/src/ops/new.rs
@@ -139,10 +139,32 @@ fn mk(
         fsx::write(
             source_path,
             indoc! {r#"
-                fn fib(a: felt252, b: felt252, n: felt252) -> felt252 {
-                    match n {
-                        0 => a,
-                        _ => fib(b, a + b, n - 1),
+                fn main() -> felt252 {
+                    fib(16)
+                }
+
+                fn fib(mut n: felt252) -> felt252 {
+                    let mut a: felt252 = 0;
+                    let mut b: felt252 = 1;
+                    loop {
+                        if n == 0 {
+                            break a;
+                        }
+                        n = n - 1;
+                        let temp = b;
+                        b = a + b;
+                        a = temp;
+                    }
+                }
+
+                #[cfg(test)]
+                mod tests {
+                    use super::fib;
+
+                    #[test]
+                    #[available_gas(100000)]
+                    fn it_works() {
+                        assert(fib(16) == 987, 'it works!');
                     }
                 }
             "#},
