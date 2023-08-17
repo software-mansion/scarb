914dbfd1a5749f78ab01039e0d13331085537136
Szymon Miśtal <37044072+szymmis@users.noreply.github.com>
2023-08-17 15:06:35 +0200

    Refactor write_script duplication in tests

--- utils/scarb-test-support/src/filesystem.rs
+++ utils/scarb-test-support/src/filesystem.rs
@@ -3,12 +3,24 @@ use std::{env, iter};
 
 use assert_fs::prelude::*;
 use assert_fs::TempDir;
+use indoc::indoc;
 use scarb::process::make_executable;
 
 pub fn write_script(name: &str, script_source: &str, t: &TempDir) {
     let script = t.child(format!("scarb-{name}{}", env::consts::EXE_SUFFIX));
     script.write_str(script_source).unwrap();
     make_executable(script.path());
+}
+
+pub fn write_simple_hello_script(name: &str, t: &TempDir) {
+    write_script(
+        name,
+        indoc! {r#"
+            #!/usr/bin/env sh
+            echo "Hello $@"
+        "#},
+        t,
+    );
 }
 
 pub fn path_with_temp_dir(t: &TempDir) -> OsString {
