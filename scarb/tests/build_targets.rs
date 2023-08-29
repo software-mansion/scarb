c0cb95482fd184ff4b70034fe094a7832e64e274
maciektr <maciej.tratnowiecki@swmansion.com>
2023-08-30 00:09:35 +0200

    Update cairo compiler revision
    commit-id:f6ebe2f5

--- scarb/tests/build_targets.rs
+++ scarb/tests/build_targets.rs
@@ -186,7 +186,7 @@ fn compile_dep_not_a_lib() {
             warn: hello v1.0.0 ([..]) ignoring invalid dependency `dep` which is missing a lib or cairo-plugin target
                Compiling hello v1.0.0 ([..])
             error: Identifier not found.
-             --> lib.cairo:1:25
+             --> [..]/lib.cairo:1:25
             fn hellp() -> felt252 { dep::forty_two() }
                                     ^*^
 
