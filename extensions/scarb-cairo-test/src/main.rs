c018df6490f1b7f3db2409c950c4b6d14b5658b9
maciektr <maciej.tratnowiecki@swmansion.com>
2023-08-30 00:11:43 +0200

    Use compiled test targets in test runner
    commit-id:e7cea12c

--- extensions/scarb-cairo-test/src/main.rs
+++ extensions/scarb-cairo-test/src/main.rs
@@ -1,20 +1,10 @@
-use std::iter;
-use std::sync::Arc;
+use std::fs;
 
-use anyhow::{bail, Result};
-use cairo_lang_compiler::db::RootDatabase;
-use cairo_lang_compiler::diagnostics::DiagnosticsReporter;
-use cairo_lang_compiler::project::{ProjectConfig, ProjectConfigContent};
-use cairo_lang_filesystem::cfg::{Cfg, CfgSet};
-use cairo_lang_filesystem::db::FilesGroup;
-use cairo_lang_filesystem::ids::{CrateLongId, Directory};
-use cairo_lang_starknet::inline_macros::selector::SelectorMacro;
-use cairo_lang_starknet::plugin::StarkNetPlugin;
-use cairo_lang_test_runner::plugin::TestPlugin;
-use cairo_lang_test_runner::TestRunner;
+use anyhow::{Context, Result};
+use cairo_lang_test_runner::{TestCompilation, TestCompilationRunner};
 use clap::Parser;
 
-use scarb_metadata::{CompilationUnitMetadata, Metadata, MetadataCommand, PackageId};
+use scarb_metadata::{CompilationUnitMetadata, Metadata, MetadataCommand, PackageId, ScarbCommand};
 use scarb_ui::args::PackagesFilter;
 
 /// Execute all unit tests of a local package.
@@ -40,118 +30,53 @@ struct Args {
 fn main() -> Result<()> {
     let args: Args = Args::parse();
 
+    ScarbCommand::new().arg("build").run()?;
+
     let metadata = MetadataCommand::new().inherit_stderr().exec()?;
 
     check_scarb_version(&metadata);
 
-    let starknet_package_id = metadata
-        .packages
-        .iter()
-        .find(|p| p.name == "starknet" && p.source.repr == "std")
-        .map(|p| p.id.clone());
+    let default_target_dir = metadata.runtime_manifest.join("target");
+    let target_dir = metadata
+        .target_dir
+        .clone()
+        .unwrap_or(default_target_dir)
+        .join("dev");
 
     for package in args.packages_filter.match_many(&metadata)? {
         println!("testing {} ...", package.name);
 
-        let Some(unit) = find_testable_compilation_unit(&metadata, &package.id) else {
-            println!("warn: package `{}` has no testable targets", package.name);
-            continue;
-        };
+        for cu in find_testable_compilation_units(&metadata, &package.id) {
+            let file_path = target_dir.join(format!("{}.test.json", cu.target.name.clone()));
+            let test_compilation = serde_json::from_str::<TestCompilation>(
+                &fs::read_to_string(file_path.clone())
+                    .with_context(|| format!("failed to read file: {file_path}"))?,
+            )
+            .with_context(|| format!("failed to deserialize compiled tests file: {file_path}"))?;
 
-        // Enable the Starknet test plugin if the package depends on the `starknet` package.
-        let starknet = if let Some(starknet_package_id) = &starknet_package_id {
-            unit.cairo_plugins
-                .iter()
-                .any(|p| p.package == *starknet_package_id)
-        } else {
-            false
-        };
-
-        let db = build_root_database(unit, starknet)?;
-
-        let main_crate_ids = unit
-            .components
-            .iter()
-            .map(|component| db.intern_crate(CrateLongId::Real(component.name.clone().into())))
-            .collect();
-
-        let test_crate_ids = vec![db.intern_crate(CrateLongId::Real(package.name.clone().into()))];
-
-        if DiagnosticsReporter::stderr().check(&db) {
-            bail!("could not compile `{}` due to previous error", package.name);
+            let runner = TestCompilationRunner::new(
+                test_compilation,
+                &args.filter,
+                args.include_ignored,
+                args.ignored,
+            );
+            runner.run()?;
+            println!();
         }
-
-        let runner = TestRunner {
-            db,
-            main_crate_ids,
-            test_crate_ids,
-            filter: args.filter.clone(),
-            include_ignored: args.include_ignored,
-            ignored: args.ignored,
-            starknet,
-        };
-        runner.run()?;
-
-        println!();
     }
 
     Ok(())
 }
 
-fn find_testable_compilation_unit<'a>(
+fn find_testable_compilation_units<'a>(
     metadata: &'a Metadata,
     package_id: &PackageId,
-) -> Option<&'a CompilationUnitMetadata> {
+) -> Vec<&'a CompilationUnitMetadata> {
     metadata
         .compilation_units
         .iter()
-        .filter(|unit| unit.package == *package_id)
-        .min_by_key(|unit| match unit.target.name.as_str() {
-            name @ "lib" => (0, name),
-            name => (1, name),
-        })
-}
-
-fn build_root_database(unit: &CompilationUnitMetadata, starknet: bool) -> Result<RootDatabase> {
-    let mut b = RootDatabase::builder();
-
-    b.with_project_config(ProjectConfig {
-        base_path: unit.target.source_root().into(),
-        corelib: unit
-            .components
-            .iter()
-            .find(|c| c.name == "core")
-            .map(|c| Directory::Real(c.source_root().into())),
-        content: ProjectConfigContent {
-            crate_roots: unit
-                .components
-                .iter()
-                .filter(|c| c.name != "core")
-                .map(|c| (c.name.clone().into(), c.source_root().into()))
-                .collect(),
-        },
-    });
-
-    b.with_cfg(
-        unit.cfg
-            .iter()
-            .map(|cfg| {
-                serde_json::to_value(cfg)
-                    .and_then(serde_json::from_value)
-                    .expect("Cairo's `Cfg` must serialize identically as Scarb Metadata's `Cfg`.")
-            })
-            .chain(iter::once(Cfg::name("test")))
-            .collect::<CfgSet>(),
-    );
-
-    b.with_macro_plugin(Arc::new(TestPlugin::default()));
-
-    if starknet {
-        b.with_macro_plugin(Arc::new(StarkNetPlugin::default()));
-        b.with_inline_macro_plugin(SelectorMacro::NAME, Arc::new(SelectorMacro));
-    }
-
-    b.build()
+        .filter(|unit| unit.package == *package_id && unit.target.kind == "test")
+        .collect::<Vec<&CompilationUnitMetadata>>()
 }
 
 fn check_scarb_version(metadata: &Metadata) {
