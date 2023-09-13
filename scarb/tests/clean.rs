bdbc0997d9e9ac9fd347e686a296eb9a44003fa1
maciektr <maciej.tratnowiecki@swmansion.com>
2023-09-13 12:28:41 +0200

    Ensure scarb clean only works in a workspace
    commit-id:485c724e

--- scarb/tests/clean.rs
+++ scarb/tests/clean.rs
@@ -1,11 +1,13 @@
 use assert_fs::prelude::*;
+use assert_fs::TempDir;
+use indoc::indoc;
 
 use scarb_test_support::command::Scarb;
 use scarb_test_support::project_builder::ProjectBuilder;
 
 #[test]
 fn simple() {
-    let t = assert_fs::TempDir::new().unwrap();
+    let t = TempDir::new().unwrap();
     ProjectBuilder::start().build(&t);
 
     Scarb::quick_snapbox()
@@ -21,4 +23,27 @@ fn simple() {
         .assert()
         .success();
     t.child("target").assert(predicates::path::missing());
+}
+
+#[test]
+fn requires_workspace() {
+    let t = TempDir::new().unwrap();
+    t.child("target/dev/some.sierra.json")
+        .write_str("Lorem ipsum.")
+        .unwrap();
+
+    Scarb::quick_snapbox()
+        .arg("clean")
+        .current_dir(&t)
+        .assert()
+        .failure()
+        .stdout_matches(indoc! {r#"
+            error: failed to read manifest at: [..]/Scarb.toml
+
+            Caused by:
+                No such file or directory (os error 2)
+        "#});
+
+    t.child("target/dev/some.sierra.json")
+        .assert(predicates::path::is_file());
 }
