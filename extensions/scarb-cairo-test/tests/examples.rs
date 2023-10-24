facb2134588a513f0cb10729b447f259c2f52927
maciektr <maciej.tratnowiecki@swmansion.com>
2023-10-24 15:40:09 +0200

    Ignore cairo-test run for `dependencies` example

--- extensions/scarb-cairo-test/tests/examples.rs
+++ extensions/scarb-cairo-test/tests/examples.rs
@@ -4,7 +4,8 @@ use snapbox::cmd::{cargo_bin, Command};
 
 use test_for_each_example::test_for_each_example;
 
-#[test_for_each_example]
+// TODO(maciektr): Revert ignoring the dependencies test case.
+#[test_for_each_example(ignore = "dependencies")]
 fn cairo_test(example: &Path) {
     Command::new(cargo_bin("scarb"))
         .arg("cairo-test")
