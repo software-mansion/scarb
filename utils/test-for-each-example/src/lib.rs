412dd102dc3e724097f4c3a87eb8e39d815c2247
maciektr <maciej.tratnowiecki@swmansion.com>
2023-10-25 10:00:55 +0200

    Allow ignoring example project by name in test_for_each_example macro (#824)
    We fail to run cairo-test on alexandria with cairo main. This results in
    failed tests on https://github.com/software-mansion/scarb/pull/821
    
    This PR adds an ignore argument on test_for_each_example, so we can skip
    running cairo-test on dependencies example.
    
    ---------
    
    Signed-off-by: maciektr <mtratnowiecki@gmail.com>
    Co-authored-by: Marek Kaput <marek.kaput@swmansion.com>
    (cherry picked from commit 59fc2d5f8d69677474e157552078723eb18baaa8)

--- utils/test-for-each-example/src/lib.rs
+++ utils/test-for-each-example/src/lib.rs
@@ -2,13 +2,46 @@ use proc_macro::TokenStream;
 use std::path::Path;
 
 use camino::Utf8PathBuf;
+use darling::ast::NestedMeta;
+use darling::{Error, FromMeta};
 use proc_macro2::{Ident, Span};
 use quote::quote;
 use syn::{parse_macro_input, parse_quote, ItemFn};
 
+#[derive(Debug, FromMeta)]
+struct TestForEachArgs {
+    /// Comma-separated list of example project dir names to ignore.
+    #[darling(default)]
+    ignore: Option<String>,
+}
+
 #[proc_macro_attribute]
-pub fn test_for_each_example(_args: TokenStream, input: TokenStream) -> TokenStream {
+pub fn test_for_each_example(args: TokenStream, input: TokenStream) -> TokenStream {
     let mut item: ItemFn = parse_macro_input!(input as ItemFn);
+
+    let attr_args = match NestedMeta::parse_meta_list(args.into()) {
+        Ok(v) => v,
+        Err(e) => {
+            return TokenStream::from(Error::from(e).write_errors());
+        }
+    };
+
+    let args = match TestForEachArgs::from_list(&attr_args) {
+        Ok(v) => v,
+        Err(e) => {
+            return TokenStream::from(e.write_errors());
+        }
+    };
+
+    let ignored = args
+        .ignore
+        .map(|ignore| {
+            ignore
+                .split(',')
+                .map(ToString::to_string)
+                .collect::<Vec<_>>()
+        })
+        .unwrap_or(vec![]);
 
     let examples_dir_path = Path::new(env!("CARGO_MANIFEST_DIR"))
         .parent()
@@ -35,9 +68,16 @@ pub fn test_for_each_example(_args: TokenStream, input: TokenStream) -> TokenStr
 
             let example_name = Ident::new(example_path.file_name().unwrap(), Span::call_site());
 
+            let ignore_attr = if ignored.contains(&example_name.to_string()) {
+                quote!(#[ignore = "test ignored by example name"])
+            } else {
+                quote!()
+            };
+
             let example_path = example_path.as_str();
 
             let test = quote! {
+                #ignore_attr
                 #[::core::prelude::v1::test]
                 fn #example_name() {
                     let example_path = ::std::path::Path::new(#example_path);
