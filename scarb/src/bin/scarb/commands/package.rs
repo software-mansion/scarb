71caf50fa0af0e6da5d60e6d920527095aac01e7
Szymon Miśtal <37044072+szymmis@users.noreply.github.com>
2023-10-27 09:59:36 +0200

    Use shorter syntax for allow_dirty package option

--- scarb/src/bin/scarb/commands/package.rs
+++ scarb/src/bin/scarb/commands/package.rs
@@ -23,7 +23,7 @@ pub fn run(args: PackageArgs, config: &Config) -> Result<()> {
 
     let opts = PackageOpts {
         /// Disable dirty repository checks when printing package files.
-        allow_dirty: if args.list { true } else { args.allow_dirty },
+        allow_dirty: args.list || args.allow_dirty,
     };
 
     if args.list {
