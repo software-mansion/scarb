bdb3c473fae2bcdc60a2e0ee60fc1faf6c785f28
maciektr <maciej.tratnowiecki@swmansion.com>
2023-11-20 17:31:12 +0100

    Pull corelib from cargo cache for path/git builds

--- scarb/build.rs
+++ scarb/build.rs
@@ -1,21 +1,22 @@
+use fs_extra::dir::{copy, CopyOptions};
 use std::fs::File;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::process::Command;
 use std::{env, fs, io};
 
 use zip::ZipArchive;
 
-use scarb_build_metadata::{CAIRO_COMMIT_REV, CAIRO_VERSION};
+use scarb_build_metadata::{CAIRO_COMMIT_REV, CAIRO_VERSION, SCARB_CORELIB_LOCAL_PATH};
 
 fn main() {
-    download_core(CAIRO_COMMIT_REV);
+    obtain_core(CAIRO_COMMIT_REV);
 }
 
 fn is_docs_rs() -> bool {
     env::var("DOCS_RS").is_ok()
 }
 
-fn download_core(rev: &str) {
+fn obtain_core(rev: &str) {
     println!("cargo:rerun-if-env-changed=CAIRO_ARCHIVE");
     let out_dir = env::var("OUT_DIR").unwrap();
     if is_docs_rs() {
@@ -28,61 +29,77 @@ fn download_core(rev: &str) {
         );
         return;
     }
-
     let core_path = PathBuf::from_iter([&out_dir, &format!("core-{}", ident(rev))]);
     if !core_path.is_dir() {
-        let cairo_zip = PathBuf::from_iter([&out_dir, "cairo.zip"]);
-
+        let cairo_zip: PathBuf = PathBuf::from_iter([&out_dir, "cairo.zip"]);
         if let Ok(cairo_archive) = env::var("CAIRO_ARCHIVE") {
+            // If `CAIRO_ARCHIVE` env variable is specified, prefer it.
             // Copy archive to `cairo_zip`, without keeping file attributes.
             eprintln!("Copying Cairo archive from `CAIRO_ARCHIVE={cairo_archive}`.");
             let mut src = File::open(&cairo_archive).unwrap();
             let mut dst = File::create(&cairo_zip).unwrap();
             io::copy(&mut src, &mut dst).unwrap();
-        } else {
-            let url = format!("https://github.com/starkware-libs/cairo/archive/{rev}.zip");
-            let mut curl = Command::new("curl");
-            curl.args(["--proto", "=https", "--tlsv1.2", "-fL"]);
-            curl.arg("-o");
-            curl.arg(&cairo_zip);
-            curl.arg(&url);
-            eprintln!("{curl:?}");
-            let curl_exit = curl.status().expect("Failed to start curl");
-            if !curl_exit.success() {
-                panic!("Failed to download {url} with curl")
-            }
-        }
-
-        fs::create_dir_all(&core_path).unwrap();
-        let cairo_file = File::open(cairo_zip).unwrap();
-        let mut cairo_archive = ZipArchive::new(cairo_file).unwrap();
-        for i in 0..cairo_archive.len() {
-            let mut input = cairo_archive.by_index(i).unwrap();
-
-            if input.name().ends_with('/') {
-                continue;
-            }
-
-            let path = input.enclosed_name().unwrap();
-
-            let path = PathBuf::from_iter(path.components().skip(1));
-            let Ok(path) = path.strip_prefix("corelib") else {
-                continue;
+            extract_zipped_core(&cairo_zip, &core_path);
+        } else if let Some(corelib_path) = SCARB_CORELIB_LOCAL_PATH {
+            // Else, if corelib is present in Cargo cache, prefer it over remote.
+            // Copy the corelib directory.
+            let source = PathBuf::from(corelib_path);
+            let copy_opts = CopyOptions {
+                content_only: true,
+                ..Default::default()
             };
-
-            let path = core_path.join(path);
-
-            if let Some(parent) = path.parent() {
-                fs::create_dir_all(parent).unwrap();
-            }
-
-            let mut output = File::create(path).unwrap();
-            io::copy(&mut input, &mut output).unwrap();
+            copy(source, &core_path, &copy_opts).unwrap();
+        } else {
+            // If none of the above applies, download corelib from remote.
+            download_remote(&cairo_zip, rev);
+            extract_zipped_core(&cairo_zip, &core_path);
         }
     }
-
     check_corelib_version(core_path.join("Scarb.toml"));
     println!("cargo:rustc-env=SCARB_CORE_PATH={}", core_path.display());
+}
+
+fn download_remote(cairo_zip: &Path, rev: &str) {
+    let url: String = format!("https://github.com/starkware-libs/cairo/archive/{rev}.zip");
+    let mut curl = Command::new("curl");
+    curl.args(["--proto", "=https", "--tlsv1.2", "-fL"]);
+    curl.arg("-o");
+    curl.arg(cairo_zip);
+    curl.arg(&url);
+    eprintln!("{curl:?}");
+    let curl_exit = curl.status().expect("Failed to start curl");
+    if !curl_exit.success() {
+        panic!("Failed to download {url} with curl")
+    }
+}
+
+fn extract_zipped_core(cairo_zip: &Path, core_path: &Path) {
+    fs::create_dir_all(core_path).unwrap();
+    let cairo_file = File::open(cairo_zip).unwrap();
+    let mut cairo_archive = ZipArchive::new(cairo_file).unwrap();
+    for i in 0..cairo_archive.len() {
+        let mut input = cairo_archive.by_index(i).unwrap();
+
+        if input.name().ends_with('/') {
+            continue;
+        }
+
+        let path = input.enclosed_name().unwrap();
+
+        let path = PathBuf::from_iter(path.components().skip(1));
+        let Ok(path) = path.strip_prefix("corelib") else {
+            continue;
+        };
+
+        let path = core_path.join(path);
+
+        if let Some(parent) = path.parent() {
+            fs::create_dir_all(parent).unwrap();
+        }
+
+        let mut output = File::create(path).unwrap();
+        io::copy(&mut input, &mut output).unwrap();
+    }
 }
 
 fn ident(id: &str) -> String {
--- utils/scarb-build-metadata/src/lib.rs
+++ utils/scarb-build-metadata/src/lib.rs
@@ -25,7 +25,14 @@ pub const CAIRO_COMMIT_HASH: Option<CommitHash> = match (
     (None, None) => None,
     _ => panic!("Either SCARB_CAIRO_COMMIT_HASH or SCARB_CAIRO_SHORT_COMMIT_HASH is missing."),
 };
+
+// Commit hash corresponding to cairo compiler semver version.
 pub const CAIRO_COMMIT_REV: &str = env!("SCARB_CAIRO_COMMIT_REV");
+// Optional path to corelib from local cargo cache.
+//
+// If `None`, that the corelib should be attempted to be downloaded from Cairo
+// repository on GitHub.
+pub const SCARB_CORELIB_LOCAL_PATH: Option<&str> = option_env!("SCARB_CORELIB_LOCAL_PATH");
 
 #[cfg(test)]
 mod tests {
