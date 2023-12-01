fa7e541514ccf320ba3da8b9e0182dadaf5c6ff7
maciektr <maciej.tratnowiecki@swmansion.com>
2023-12-01 11:01:33 +0100

    Use diagnostic severity in cairo formatter
    commit-id:d7f87008

--- scarb/tests/fmt.rs
+++ scarb/tests/fmt.rs
@@ -132,7 +132,6 @@ fn simple_format_with_parsing_error() {
         fn main()    ->    {      42      }
                        ^
 
-        Error writing files: cannot parse [..]lib.cairo
         "#});
 }
 
