d8109230deb0ff84a557431cd4590fa30d9c9525
maciektr <maciej.tratnowiecki@swmansion.com>
2023-12-21 14:19:54 +0100

    Revert severity in test collector

--- extensions/scarb-snforge-test-collector/src/compilation/test_collector/config.rs
+++ extensions/scarb-snforge-test-collector/src/compilation/test_collector/config.rs
@@ -1,7 +1,6 @@
 use anyhow::Result;
 use cairo_felt::Felt252;
 use cairo_lang_defs::plugin::PluginDiagnostic;
-use cairo_lang_diagnostics::Severity;
 use cairo_lang_syntax::attribute::structured::{Attribute, AttributeArg, AttributeArgVariant};
 use cairo_lang_syntax::node::ast::{ArgClause, Expr, PathSegment};
 use cairo_lang_syntax::node::db::SyntaxGroup;
@@ -103,7 +102,6 @@ pub fn forge_try_extract_test_config(
     if maybe_test_config.is_none() {
         for attr in [fork_attr, fuzzer_attr].into_iter().flatten() {
             diagnostics.push(PluginDiagnostic {
-                severity: Severity::Error,
                 stable_ptr: attr.id_stable_ptr.untyped(),
                 message: "Attribute should only appear on tests.".into(),
             });
@@ -116,7 +114,6 @@ pub fn forge_try_extract_test_config(
         } else {
             extract_fork_config(db, attr).on_none(|| {
                 diagnostics.push(PluginDiagnostic {
-                    severity: Severity::Error,
                     stable_ptr: attr.args_stable_ptr.untyped(),
                     message: "Expected fork config must be of the form `url: <double quote \
                                   string>, block_id: <snforge_std::BlockId>`."
@@ -131,7 +128,6 @@ pub fn forge_try_extract_test_config(
     let fuzzer_config = if let Some(attr) = fuzzer_attr {
         extract_fuzzer_config(db, attr).on_none(|| {
             diagnostics.push(PluginDiagnostic {
-                severity: Severity::Error,
                 stable_ptr: attr.args_stable_ptr.untyped(),
                 message: "Expected fuzzer config must be of the form `runs: <u32>, seed: <u64>`"
                     .into(),
