c531a6e509dbb8b43f1c2d37f9d74a7b1bf7243d
Marek Kaput <marek.kaput@swmansion.com>
2024-01-22 15:02:58 +0100

    Prepare release `2.5.0` (#1077)
    Signed-off-by: Marek Kaput <marek.kaput@swmansion.com>

--- extensions/scarb-snforge-test-collector/src/compilation/test_collector/plugin.rs
+++ extensions/scarb-snforge-test-collector/src/compilation/test_collector/plugin.rs
@@ -1,4 +1,4 @@
-use cairo_lang_defs::plugin::{MacroPlugin, PluginResult};
+use cairo_lang_defs::plugin::{MacroPlugin, MacroPluginMetadata, PluginResult};
 use cairo_lang_semantic::plugin::PluginSuite;
 use cairo_lang_syntax::attribute::structured::AttributeListStructurize;
 use cairo_lang_syntax::node::ast;
@@ -13,7 +13,12 @@ use super::forge_try_extract_test_config;
 struct TestPlugin;
 
 impl MacroPlugin for TestPlugin {
-    fn generate_code(&self, db: &dyn SyntaxGroup, item_ast: ast::ModuleItem) -> PluginResult {
+    fn generate_code(
+        &self,
+        db: &dyn SyntaxGroup,
+        item_ast: ast::ModuleItem,
+        _metadata: &MacroPluginMetadata<'_>,
+    ) -> PluginResult {
         PluginResult {
             code: None,
             diagnostics: if let ast::ModuleItem::FreeFunction(free_func_ast) = item_ast {
