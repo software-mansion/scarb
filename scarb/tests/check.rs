deacd227fff6b3fce0c9e44185a070e464d13c89
Tomasz Miśkowicz <tomasz.miskowicz@swmansion.com>
2024-01-22 16:08:20 +0100

    rename failing test

--- scarb/tests/check.rs
+++ scarb/tests/check.rs
@@ -36,7 +36,7 @@ fn check_simple() {
 }
 
 #[test]
-fn compile_with_syntax_error() {
+fn check_fail_with_syntax_error() {
     let t = TempDir::new().unwrap();
     ProjectBuilder::start()
         .name("hello")
@@ -51,11 +51,10 @@ fn compile_with_syntax_error() {
         .code(1)
         .stdout_matches(indoc! {r#"
                 Checking hello v0.1.0 ([..]Scarb.toml)
-            error: Skipped tokens. Expected: Const/Module/Use/FreeFunction/ExternFunction/ExternType/Trait/Impl/Struct/Enum/TypeAlias/InlineMacro or an attribute.
+            error: Skipped tokens. Expected: Const/Enum/ExternFunction/ExternType/Function/Impl/InlineMacro/Module/Struct/Trait/TypeAlias/Use or an attribute.
              --> [..]/lib.cairo:1:1
             not_a_keyword
             ^***********^
-
 
             error: could not check `hello` due to previous error
         "#});
