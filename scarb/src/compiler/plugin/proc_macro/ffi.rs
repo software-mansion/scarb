9ab7b9db5c0e946e00b20e3fa5524a10a6bc4bbb
maciektr <maciej.tratnowiecki@swmansion.com>
2024-01-29 11:21:42 +0100

    Implement loading shared libraries for proc macro plugins
    Resolves #1128
    
    commit-id:a3155bbf

--- scarb/src/compiler/plugin/proc_macro/ffi.rs
+++ scarb/src/compiler/plugin/proc_macro/ffi.rs
@@ -1,10 +1,21 @@
-use crate::core::{Package, PackageId};
-use anyhow::Result;
+use crate::core::{Config, Package, PackageId};
+use anyhow::{Context, Result};
 use cairo_lang_defs::patcher::PatchBuilder;
+use cairo_lang_macro::stable_abi::{StableProcMacroResult, StableTokenStream};
 use cairo_lang_macro::{ProcMacroResult, TokenStream};
 use cairo_lang_syntax::node::db::SyntaxGroup;
 use cairo_lang_syntax::node::{ast, TypedSyntaxNode};
+use camino::Utf8PathBuf;
+use libloading::{Library, Symbol};
 use std::fmt::Debug;
+
+use crate::compiler::plugin::proc_macro::compilation::SharedLibraryProvider;
+#[cfg(not(windows))]
+use libloading::os::unix::Symbol as RawSymbol;
+#[cfg(windows)]
+use libloading::os::windows::Symbol as RawSymbol;
+
+pub const PROC_MACRO_BUILD_PROFILE: &str = "release";
 
 pub trait FromItemAst {
     fn from_item_ast(db: &dyn SyntaxGroup, item_ast: ast::ModuleItem) -> Self;
@@ -22,9 +33,17 @@ impl FromItemAst for TokenStream {
 ///
 /// This struct is a wrapper around a shared library containing the procedural macro implementation.
 /// It is responsible for loading the shared library and providing a safe interface for code expansion.
-#[derive(Debug, Clone)]
 pub struct ProcMacroInstance {
     package_id: PackageId,
+    plugin: Plugin,
+}
+
+impl Debug for ProcMacroInstance {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("ProcMacroInstance")
+            .field("package_id", &self.package_id)
+            .finish()
+    }
 }
 
 impl ProcMacroInstance {
@@ -32,21 +51,54 @@ impl ProcMacroInstance {
         self.package_id
     }
 
-    pub fn try_new(package: Package) -> Result<Self> {
-        // Load shared library
-        // TODO(maciektr): Implement
+    /// Load shared library
+    pub fn try_new(package: Package, config: &Config) -> Result<Self> {
+        let lib_path = package.shared_lib_path(config);
+        let plugin = unsafe { Plugin::try_new(lib_path.to_path_buf())? };
         Ok(Self {
+            plugin,
             package_id: package.id,
         })
     }
-
     pub fn declared_attributes(&self) -> Vec<String> {
         vec![self.package_id.name.to_string()]
     }
 
-    pub(crate) fn generate_code(&self, _token_stream: TokenStream) -> ProcMacroResult {
-        // Apply expansion to token stream.
-        // TODO(maciektr): Implement
-        ProcMacroResult::Leave
+    /// Apply expansion to token stream.
+    pub(crate) fn generate_code(&self, token_stream: TokenStream) -> ProcMacroResult {
+        let ffi_token_stream = unsafe { StableTokenStream::from_token_stream(token_stream) };
+        let result = (self.plugin.vtable.expand)(ffi_token_stream);
+        unsafe { result.into_proc_macro_result() }
+    }
+}
+
+type ExpandCode = extern "C" fn(StableTokenStream) -> StableProcMacroResult;
+
+struct VTableV0 {
+    expand: RawSymbol<ExpandCode>,
+}
+
+impl VTableV0 {
+    unsafe fn try_new(library: &Library) -> Result<VTableV0> {
+        let expand: Symbol<'_, ExpandCode> = library
+            .get(b"expand\0")
+            .context("failed to load expand function for procedural macro")?;
+        let expand = expand.into_raw();
+        Ok(VTableV0 { expand })
+    }
+}
+
+struct Plugin {
+    #[allow(dead_code)]
+    library: Library,
+    vtable: VTableV0,
+}
+
+impl Plugin {
+    unsafe fn try_new(library_path: Utf8PathBuf) -> Result<Plugin> {
+        let library = Library::new(library_path)?;
+        let vtable = VTableV0::try_new(&library)?;
+
+        Ok(Plugin { library, vtable })
     }
 }
--- scarb/src/compiler/plugin/proc_macro/mod.rs
+++ scarb/src/compiler/plugin/proc_macro/mod.rs
@@ -1,3 +1,4 @@
+pub mod compilation;
 mod ffi;
 mod host;
 
