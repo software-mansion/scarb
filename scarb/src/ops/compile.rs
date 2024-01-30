98176dbcb2287f6d5a7f097a3b4f247415ab2179
Tomasz Miśkowicz <tomasz.miskowicz@swmansion.com>
2024-01-30 14:54:25 +0100

    Add `scarb check` (#1046)
    Closes #137

--- scarb/src/ops/compile.rs
+++ scarb/src/ops/compile.rs
@@ -1,4 +1,5 @@
 use anyhow::{anyhow, Result};
+use cairo_lang_compiler::db::RootDatabase;
 use cairo_lang_compiler::diagnostics::DiagnosticsError;
 use indoc::formatdoc;
 
@@ -6,8 +7,9 @@ use scarb_ui::components::Status;
 use scarb_ui::HumanDuration;
 
 use crate::compiler::db::{build_scarb_root_database, has_starknet_plugin};
+use crate::compiler::helpers::build_compiler_config;
 use crate::compiler::CompilationUnit;
-use crate::core::{PackageId, TargetKind, Utf8PathWorkspaceExt, Workspace};
+use crate::core::{PackageId, PackageName, TargetKind, Utf8PathWorkspaceExt, Workspace};
 use crate::ops;
 
 #[derive(Debug)]
@@ -18,6 +20,25 @@ pub struct CompileOpts {
 
 #[tracing::instrument(skip_all, level = "debug")]
 pub fn compile(packages: Vec<PackageId>, opts: CompileOpts, ws: &Workspace<'_>) -> Result<()> {
+    process(packages, opts, ws, compile_unit, None)
+}
+
+#[tracing::instrument(skip_all, level = "debug")]
+pub fn check(packages: Vec<PackageId>, opts: CompileOpts, ws: &Workspace<'_>) -> Result<()> {
+    process(packages, opts, ws, check_unit, Some("checking"))
+}
+
+#[tracing::instrument(skip_all, level = "debug")]
+fn process<F>(
+    packages: Vec<PackageId>,
+    opts: CompileOpts,
+    ws: &Workspace<'_>,
+    mut operation: F,
+    operation_type: Option<&str>,
+) -> Result<()>
+where
+    F: FnMut(CompilationUnit, &Workspace<'_>) -> Result<()>,
+{
     let resolve = ops::resolve_workspace(ws)?;
 
     // Add test compilation units to build
@@ -46,14 +67,17 @@ pub fn compile(packages: Vec<PackageId>, opts: CompileOpts, ws: &Workspace<'_>) 
         .collect::<Vec<_>>();
 
     for unit in compilation_units {
-        compile_unit(unit, ws)?;
+        operation(unit, ws)?;
     }
 
     let elapsed_time = HumanDuration(ws.config().elapsed_time());
-    ws.config().ui().print(Status::new(
-        "Finished",
-        &format!("release target(s) in {elapsed_time}"),
-    ));
+    let formatted_message = match operation_type {
+        Some(op) => format!("{op} release target(s) in {elapsed_time}"),
+        None => format!("release target(s) in {elapsed_time}"),
+    };
+    ws.config()
+        .ui()
+        .print(Status::new("Finished", &formatted_message));
 
     Ok(())
 }
@@ -67,24 +91,7 @@ fn compile_unit(unit: CompilationUnit, ws: &Workspace<'_>) -> Result<()> {
 
     let mut db = build_scarb_root_database(&unit, ws)?;
 
-    // NOTE: This is a special case that can be hit frequently by newcomers. Not specifying
-    //   `starknet` dependency will error in 99% real-world Starknet contract projects.
-    //   I think we can get away with emitting false positives for users who write raw contracts
-    //   without using Starknet code generators. Such people shouldn't do what they do 😁
-    if unit.target().kind == TargetKind::STARKNET_CONTRACT && !has_starknet_plugin(&db) {
-        ws.config().ui().warn(formatdoc! {
-            r#"
-            package `{package_name}` declares `starknet-contract` target, but does not depend on `starknet` package
-            note: this may cause contract compilation to fail with cryptic errors
-            help: add dependency on `starknet` to package manifest
-             --> {scarb_toml}
-                [dependencies]
-                starknet = ">={cairo_version}"
-            "#,
-            scarb_toml=unit.main_component().package.manifest_path().workspace_relative(ws),
-            cairo_version = crate::version::get().cairo.version,
-        })
-    }
+    check_starknet_dependency(&unit, ws, &db, &package_name);
 
     ws.config()
         .compilers()
@@ -98,6 +105,60 @@ fn compile_unit(unit: CompilationUnit, ws: &Workspace<'_>) -> Result<()> {
         })?;
 
     Ok(())
+}
+
+fn check_unit(unit: CompilationUnit, ws: &Workspace<'_>) -> Result<()> {
+    let package_name = unit.main_package_id.name.clone();
+
+    ws.config()
+        .ui()
+        .print(Status::new("Checking", &unit.name()));
+
+    let db = build_scarb_root_database(&unit, ws)?;
+
+    check_starknet_dependency(&unit, ws, &db, &package_name);
+
+    let mut compiler_config = build_compiler_config(&unit, ws);
+
+    compiler_config
+        .diagnostics_reporter
+        .ensure(&db)
+        .map_err(|err| {
+            let valid_error = err.into();
+            if !suppress_error(&valid_error) {
+                ws.config().ui().anyhow(&valid_error);
+            }
+
+            anyhow!("could not check `{package_name}` due to previous error")
+        })?;
+
+    Ok(())
+}
+
+fn check_starknet_dependency(
+    unit: &CompilationUnit,
+    ws: &Workspace<'_>,
+    db: &RootDatabase,
+    package_name: &PackageName,
+) {
+    // NOTE: This is a special case that can be hit frequently by newcomers. Not specifying
+    //   `starknet` dependency will error in 99% real-world Starknet contract projects.
+    //   I think we can get away with emitting false positives for users who write raw contracts
+    //   without using Starknet code generators. Such people shouldn't do what they do 😁
+    if unit.target().kind == TargetKind::STARKNET_CONTRACT && !has_starknet_plugin(db) {
+        ws.config().ui().warn(formatdoc! {
+            r#"
+            package `{package_name}` declares `starknet-contract` target, but does not depend on `starknet` package
+            note: this may cause contract compilation to fail with cryptic errors
+            help: add dependency on `starknet` to package manifest
+             --> {scarb_toml}
+                [dependencies]
+                starknet = ">={cairo_version}"
+            "#,
+            scarb_toml=unit.main_component().package.manifest_path().workspace_relative(ws),
+            cairo_version = crate::version::get().cairo.version,
+        })
+    }
 }
 
 fn suppress_error(err: &anyhow::Error) -> bool {
