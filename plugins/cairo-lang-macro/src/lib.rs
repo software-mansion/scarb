c5e9e086db5fb13af23b0d1fcf160851af3ca4c1
maciektr <maciej.tratnowiecki@swmansion.com>
2024-02-29 15:58:26 +0100

    Add aux_data to proc macro result (#1143)
    commit-id:89f844e2
    
    ---
    
    **Stack**:
    - #1165
    - #1161
    - #1159
    - #1157
    - #1143 ⬅
    
    
    ⚠️ *Part of a stack created by [spr](https://github.com/ejoffe/spr). Do
    not merge manually using the UI - doing so may have unexpected results.*

--- plugins/cairo-lang-macro/src/lib.rs
+++ plugins/cairo-lang-macro/src/lib.rs
@@ -10,7 +10,10 @@ pub enum ProcMacroResult {
     /// Plugin has not taken any action.
     Leave,
     /// Plugin generated [`TokenStream`] replacement.
-    Replace(TokenStream),
+    Replace {
+        token_stream: TokenStream,
+        aux_data: Option<AuxData>,
+    },
     /// Plugin ordered item removal.
     Remove,
 }
@@ -26,6 +29,26 @@ impl TokenStream {
 }
 
 impl Display for TokenStream {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+/// Auxiliary data returned by procedural macro.
+#[derive(Debug)]
+pub struct AuxData(String);
+
+impl AuxData {
+    pub fn new(s: String) -> Self {
+        Self(s)
+    }
+
+    pub fn try_new<T: serde::Serialize>(value: T) -> Result<Self, serde_json::Error> {
+        Ok(Self(serde_json::to_string(&value)?))
+    }
+}
+
+impl Display for AuxData {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.0)
     }
