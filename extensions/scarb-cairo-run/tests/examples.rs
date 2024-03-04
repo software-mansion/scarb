781df2aa5b5bc4525cd210e2326920964dcfbe4d
maciektr <maciej.tratnowiecki@swmansion.com>
2024-03-04 13:51:22 +0100

    Update scarb new/init hello world Cairo code (#1169)

--- extensions/scarb-cairo-run/tests/examples.rs
+++ extensions/scarb-cairo-run/tests/examples.rs
@@ -97,7 +97,7 @@ fn can_limit_gas() {
                 Finished release target(s) in [..]
                  Running hello_world
             Run completed successfully, returning [987]
-            Remaining gas: 68340
+            Remaining gas: 50760
         "#});
 }
 
--- scarb/src/ops/new.rs
+++ scarb/src/ops/new.rs
@@ -156,22 +156,20 @@ fn mk(
         fsx::write(
             source_path,
             indoc! {r#"
-                fn main() -> felt252 {
+                fn main() -> u32 {
                     fib(16)
                 }
 
-                fn fib(mut n: felt252) -> felt252 {
-                    let mut a: felt252 = 0;
-                    let mut b: felt252 = 1;
-                    loop {
-                        if n == 0 {
-                            break a;
-                        }
+                fn fib(mut n: u32) -> u32 {
+                    let mut a: u32 = 0;
+                    let mut b: u32 = 1;
+                    while n != 0 {
                         n = n - 1;
                         let temp = b;
                         b = a + b;
                         a = temp;
-                    }
+                    };
+                    a
                 }
 
                 #[cfg(test)]
