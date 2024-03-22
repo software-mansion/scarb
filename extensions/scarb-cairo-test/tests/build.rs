027954b88b0f2985e0025684199b0ec18e2a32eb
maciektr <maciej.tratnowiecki@swmansion.com>
2024-03-22 20:14:02 +0100

    Downstream cairo-test option to print used resources for tests (cairo… (#1221)
    …#5266)
    
    commit-id:3c5ee860
    
    ---
    
    **Stack**:
    - #1220
    - #1219
    - #1218
    - #1217
    - #1216
    - #1215
    - #1214
    - #1221 ⬅
    
    
    ⚠️ *Part of a stack created by [spr](https://github.com/ejoffe/spr). Do
    not merge manually using the UI - doing so may have unexpected results.*

--- extensions/scarb-cairo-test/tests/build.rs
+++ extensions/scarb-cairo-test/tests/build.rs
@@ -53,3 +53,56 @@ fn can_test_without_gas() {
 
         "#});
 }
+
+#[test]
+fn can_print_test_resources() {
+    let t = TempDir::new().unwrap();
+    ProjectBuilder::start()
+        .name("hello")
+        .lib_cairo(indoc! {r#"
+            fn main() -> u32 {
+                fib(16)
+            }
+
+            fn fib(mut n: u32) -> u32 {
+                let mut a: u32 = 0;
+                let mut b: u32 = 1;
+                while n != 0 {
+                    n = n - 1;
+                    let temp = b;
+                    b = a + b;
+                    a = temp;
+                };
+                a
+            }
+
+            #[cfg(test)]
+            mod tests {
+                use super::fib;
+
+                #[test]
+                fn it_works() {
+                    assert(fib(16) == 987, 'it works!');
+                }
+            }
+        "#})
+        .build(&t);
+    Scarb::quick_snapbox()
+        .arg("cairo-test")
+        .arg("--print-resource-usage")
+        .current_dir(&t)
+        .assert()
+        .success()
+        .stdout_matches(indoc! {r#"
+            [..]Compiling test(hello_unittest) hello v1.0.0 ([..]Scarb.toml)
+            [..]Finished release target(s) in [..]
+            testing hello ...
+            running 1 test
+            test hello::tests::it_works ... ok (gas usage est.: [..])
+                steps: [..]
+                memory holes: [..]
+                builtins: ("range_check_builtin": [..])
+            test result: ok. 1 passed; 0 failed; 0 ignored; 0 filtered out;
+
+        "#});
+}
