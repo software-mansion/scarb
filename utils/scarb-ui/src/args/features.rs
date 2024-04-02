4bda1221c24d761df45ae98f4384e5fa1c4bec6b
Filip Krawczyk <kotmawszy@gmail.com>
2024-04-02 22:17:10 +0200

    Disallow --no-default-features and --all-features to be enabled simultaneously

--- utils/scarb-ui/src/args/features.rs
+++ utils/scarb-ui/src/args/features.rs
@@ -8,10 +8,20 @@ pub struct FeaturesSpec {
     pub features: Vec<String>,
 
     /// Activate all available features.
-    #[arg(long, default_value_t = false, env = "SCARB_ALL_FEATURES")]
+    #[arg(
+        long,
+        default_value_t = false,
+        env = "SCARB_ALL_FEATURES",
+        conflicts_with = "no_default_features"
+    )]
     pub all_features: bool,
 
     /// Do not activate the `default` feature.
-    #[arg(long, default_value_t = false, env = "SCARB_NO_DEFAULT_FEATURES")]
+    #[arg(
+        long,
+        default_value_t = false,
+        env = "SCARB_NO_DEFAULT_FEATURES",
+        conflicts_with = "all_features"
+    )]
     pub no_default_features: bool,
 }
