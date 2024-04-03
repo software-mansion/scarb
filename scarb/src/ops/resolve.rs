9d28fa631acbc7b3bb3de96c093f76b1c4b2b5c0
Bartosz Nowak <bartekn337@gmail.com>
2024-04-03 19:34:12 +0200

    Update scarb/src/ops/resolve.rs
    Co-authored-by: Marek Kaput <marek.kaput@outlook.com>
    Signed-off-by: Bartosz Nowak <bartekn337@gmail.com>

--- scarb/src/ops/resolve.rs
+++ scarb/src/ops/resolve.rs
@@ -373,7 +373,7 @@ fn get_cfg_with_features(
         .collect_vec();
 
     if !not_found_features.is_empty() {
-        bail!("Unknown features: {}", not_found_features.iter().join(", "));
+        bail!("unknown features: {}", not_found_features.iter().join(", "));
     }
 
     available_features
