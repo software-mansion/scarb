ad312fe6f6f74a972e1919e83ebbce9ef11e82a9
Bartosz Nowak <bartekn337@gmail.com>
2024-04-04 08:37:02 +0200

    manifest tests

--- scarb/tests/features.rs
+++ scarb/tests/features.rs
@@ -28,6 +28,45 @@ fn build_example_program(t: &TempDir) {
         .build(t);
 }
 
+fn build_missing_manifest_example_program(t: &TempDir) {
+    ProjectBuilder::start()
+        .name("hello")
+        .lib_cairo(indoc! {r#"
+            #[cfg(feature: 'x')]
+            fn f() -> felt252 { 21 }
+
+            #[cfg(feature: 'y')]
+            fn f() -> felt252 { 59 }
+
+            fn main() -> felt252 {
+                f()
+            }
+        "#})
+        .build(t);
+}
+
+fn build_incorrect_manifest_feature_example_program(t: &TempDir) {
+    ProjectBuilder::start()
+        .name("hello")
+        .manifest_extra(indoc! {r#"
+            [features]
+            8x = []
+            y = []
+            "#})
+        .lib_cairo(indoc! {r#"
+            #[cfg(feature: '8x')]
+            fn f() -> felt252 { 21 }
+
+            #[cfg(feature: 'y')]
+            fn f() -> felt252 { 59 }
+
+            fn main() -> felt252 {
+                f()
+            }
+        "#})
+        .build(t);
+}
+
 #[test]
 fn features_success() {
     let t = TempDir::new().unwrap();
@@ -91,6 +130,43 @@ fn features_unknown_feature() {
         .current_dir(&t)
         .assert()
         .stdout_matches("error: unknown features: z\n")
+        .failure();
+}
+
+#[test]
+fn features_fail_missing_manifest() {
+    let t = TempDir::new().unwrap();
+    build_missing_manifest_example_program(&t);
+    Scarb::quick_snapbox()
+        .arg("build")
+        .arg("--features")
+        .arg("x")
+        .current_dir(&t)
+        .assert()
+        .stdout_matches("error: no features in manifest\n")
+        .failure();
+}
+
+#[test]
+fn features_fail_incorrect_manifest() {
+    let t = TempDir::new().unwrap();
+    build_incorrect_manifest_feature_example_program(&t);
+    Scarb::quick_snapbox()
+        .arg("build")
+        .arg("--features")
+        .arg("x")
+        .current_dir(&t)
+        .assert()
+        .stdout_matches(indoc! {r#"
+            error: failed to parse manifest at: [..]/Scarb.toml
+
+            Caused by:
+                TOML parse error at line 8, column 1
+                  |
+                8 | 8x = []
+                  | ^^
+                the name `8x` cannot be used as a package name, names cannot start with a digit
+        "#})
         .failure();
 }
 
