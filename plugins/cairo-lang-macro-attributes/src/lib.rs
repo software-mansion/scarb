112aa2ff73bd456c32351c9eadc260a31719156b
maciektr <maciej.tratnowiecki@swmansion.com>
2024-04-08 09:42:22 +0200

    Replace result enum with single struct
    commit-id:3f7d4d4c

--- plugins/cairo-lang-macro-attributes/src/lib.rs
+++ plugins/cairo-lang-macro-attributes/src/lib.rs
@@ -1,5 +1,5 @@
 use proc_macro::TokenStream;
-use quote::quote;
+use quote::{quote, ToTokens};
 use scarb_stable_hash::short_hash;
 use syn::spanned::Spanned;
 use syn::{parse_macro_input, ItemFn};
@@ -11,6 +11,20 @@ use syn::{parse_macro_input, ItemFn};
 /// Note, that this macro can be used multiple times, to define multiple independent attribute macros.
 #[proc_macro_attribute]
 pub fn attribute_macro(_args: TokenStream, input: TokenStream) -> TokenStream {
+    macro_helper(input, quote!(::cairo_lang_macro::ExpansionKind::Attr))
+}
+
+/// Constructs the inline macro implementation.
+///
+/// This macro hides the conversion to stable ABI structs from the user.
+///
+/// Note, that this macro can be used multiple times, to define multiple independent attribute macros.
+#[proc_macro_attribute]
+pub fn inline_macro(_args: TokenStream, input: TokenStream) -> TokenStream {
+    macro_helper(input, quote!(::cairo_lang_macro::ExpansionKind::Inline))
+}
+
+fn macro_helper(input: TokenStream, kind: impl ToTokens) -> TokenStream {
     let item: ItemFn = parse_macro_input!(input as ItemFn);
     let original_item_name = item.sig.ident.to_string();
     let item = hide_name(item);
@@ -20,6 +34,7 @@ pub fn attribute_macro(_args: TokenStream, input: TokenStream) -> TokenStream {
         "EXPANSIONS_DESERIALIZE_{}",
         item_name.to_string().to_uppercase()
     );
+
     let callback_link = syn::Ident::new(callback_link.as_str(), item.span());
 
     let expanded = quote! {
@@ -30,51 +45,10 @@ pub fn attribute_macro(_args: TokenStream, input: TokenStream) -> TokenStream {
         static #callback_link: ::cairo_lang_macro::ExpansionDefinition =
             ::cairo_lang_macro::ExpansionDefinition{
                 name: #original_item_name,
-                kind: ::cairo_lang_macro::ExpansionKind::Attr,
+                kind: #kind,
                 fun: #item_name,
             };
     };
-
-    TokenStream::from(expanded)
-}
-
-#[proc_macro_attribute]
-pub fn inline_macro(_args: TokenStream, input: TokenStream) -> TokenStream {
-    let item: ItemFn = parse_macro_input!(input as ItemFn);
-    let original_item_name = item.sig.ident.to_string();
-    let item = hide_name(item);
-    let item_name = &item.sig.ident;
-
-    let callback_link = format!(
-        "EXPANSIONS_DESERIALIZE_{}",
-        item_name.to_string().to_uppercase()
-    );
-    let callback_link = syn::Ident::new(callback_link.as_str(), item.span());
-
-    // We wrap original function with `InlineProcMacroResult` to `ProcMacroResult` conversion.
-    // This stems from the fact, that `ProcMacroResult::Remove` does not really make sense for inline macros.
-    let wrapper_name = format!("{item_name}_inline_wrapper");
-    let wrapper_name = syn::Ident::new(wrapper_name.as_str(), item.span());
-
-    let expanded = quote! {
-        #item
-
-        fn #wrapper_name(token_stream: ::cairo_lang_macro::TokenStream) -> ::cairo_lang_macro::ProcMacroResult {
-            // Assign function pointer, to validate type.
-            let f: fn(::cairo_lang_macro::TokenStream) -> ::cairo_lang_macro::InlineProcMacroResult = #item_name;
-            f(token_stream).into()
-        }
-
-        #[::cairo_lang_macro::linkme::distributed_slice(::cairo_lang_macro::MACRO_DEFINITIONS_SLICE)]
-        #[linkme(crate = ::cairo_lang_macro::linkme)]
-        static #callback_link: ::cairo_lang_macro::ExpansionDefinition =
-            ::cairo_lang_macro::ExpansionDefinition{
-                name: #original_item_name,
-                kind: ::cairo_lang_macro::ExpansionKind::Inline,
-                fun: #wrapper_name,
-            };
-    };
-
     TokenStream::from(expanded)
 }
 
