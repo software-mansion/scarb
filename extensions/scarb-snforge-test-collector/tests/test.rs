e864391ffa244f7443ef90ec04d6e78dc39ce3c4
Piotr Magiera <56825108+piotmag769@users.noreply.github.com>
2024-04-10 17:10:19 +0200

    Add statements mappings to snforge test artifacts (#1241)

--- extensions/scarb-snforge-test-collector/tests/test.rs
+++ extensions/scarb-snforge-test-collector/tests/test.rs
@@ -442,3 +442,33 @@ fn does_not_compile_tests_in_dependencies() {
     Error: Failed to compile test artifact, for detailed information go through the logs above
     "#}));
 }
+
+#[test]
+fn generates_statements_functions_mappings() {
+    let t = TempDir::new().unwrap();
+
+    ProjectBuilder::start()
+        .name("forge_test")
+        .lib_cairo(SIMPLE_TEST)
+        .manifest_extra(indoc! {r#"
+        [cairo]
+        unstable-add-statements-functions-debug-info = true
+        "#})
+        .build(&t);
+
+    Scarb::quick_snapbox()
+        .arg("snforge-test-collector")
+        .current_dir(&t)
+        .assert()
+        .success();
+
+    let snforge_sierra = t
+        .child("target/dev/snforge/forge_test.snforge_sierra.json")
+        .read_to_string();
+
+    let json: Value = serde_json::from_str(&snforge_sierra).unwrap();
+
+    assert!(&json[0]["sierra_program"]["debug_info"]["annotations"]
+        ["github.com/software-mansion/cairo-profiler"]["statements_functions"]
+        .is_object());
+}
