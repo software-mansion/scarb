beb01c0f2c0badf3ace6ab537c458514d19090c4
drknzz <kamil.jankowski.x@gmail.com>
2024-04-29 10:52:54 +0200

    Use TempDir

--- extensions/scarb-doc/tests/test.rs
+++ extensions/scarb-doc/tests/test.rs
@@ -1,11 +1,22 @@
+use std::fs;
+
+use assert_fs::TempDir;
 use scarb_test_support::command::Scarb;
 
 #[test]
 fn test_main() {
+    let tempdir = TempDir::new().unwrap();
+    fs::copy(
+        "tests/hello_world.cairo",
+        tempdir.path().join("hello_world.cairo"),
+    )
+    .unwrap();
+
     let output = Scarb::quick_snapbox()
         .arg("doc")
         .arg("--crate-path")
-        .arg("tests/hello_world.cairo")
+        .arg("hello_world.cairo")
+        .current_dir(tempdir.path())
         .assert()
         .success();
     let stdout = std::str::from_utf8(&output.get_output().stdout).unwrap();
