0a36ebc2313871155c0703fcc2c41eae960e430b
Kamil Jankowski <kamil.jankowski.x@gmail.com>
2024-06-04 17:39:41 +0200

    [scarb-doc] Add initial crate structures (#1309)
    Next PR will add nested attributes to the structures:
    - struct members
    - enum variants
    - impl items
    - trait items

--- extensions/scarb-doc/src/main.rs
+++ extensions/scarb-doc/src/main.rs
@@ -1,31 +1,34 @@
 use anyhow::Result;
-use cairo_lang_compiler::project::{AllCratesConfig, ProjectConfig, ProjectConfigContent};
-use cairo_lang_filesystem::cfg::{Cfg, CfgSet};
-use cairo_lang_filesystem::db::{CrateSettings, Edition, ExperimentalFeaturesConfig, FilesGroup};
 use clap::Parser;
-use scarb_metadata::{
-    CompilationUnitComponentMetadata, CompilationUnitMetadata, Metadata, MetadataCommand,
-    PackageId, PackageMetadata,
-};
+
+use scarb_metadata::MetadataCommand;
 use scarb_ui::args::PackagesFilter;
-use smol_str::{SmolStr, ToSmolStr};
-use std::collections::HashMap;
-use std::path::PathBuf;
 
 use cairo_lang_compiler::db::RootDatabase;
-use cairo_lang_utils::ordered_hash_map::OrderedHashMap;
+use cairo_lang_filesystem::db::FilesGroup;
+use cairo_lang_filesystem::ids::CrateLongId;
 
-use cairo_lang_defs::db::DefsGroup;
-use cairo_lang_defs::ids::{LookupItemId, ModuleItemId, NamedLanguageElementId};
+use compilation::get_project_config;
+use types::Crate;
 
-use cairo_lang_filesystem::ids::{CrateLongId, Directory};
-use cairo_lang_semantic::db::SemanticGroup;
+mod compilation;
+mod types;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
     #[command(flatten)]
     packages_filter: PackagesFilter,
+}
+
+macro_rules! print_names {
+    ($label:expr, $var:expr) => {
+        println!(
+            "{}: {:?}",
+            $label,
+            $var.iter().map(|x| &x.item_data.name).collect::<Vec<_>>()
+        );
+    };
 }
 
 fn main() -> Result<()> {
@@ -42,166 +45,38 @@ fn main() -> Result<()> {
         b.build()?
     };
 
-    let main_crate_id = db.intern_crate(CrateLongId::Real(package_metadata.name.into()));
+    let main_crate_id = db.intern_crate(CrateLongId::Real(package_metadata.name.clone().into()));
+    let crate_ = Crate::new(db, main_crate_id);
 
-    let crate_modules = db.crate_modules(main_crate_id);
-
-    let mut item_documentation = HashMap::new();
-
-    for module_id in crate_modules.iter() {
-        let module_items = db.module_items(*module_id).unwrap();
-
-        for item in module_items.iter() {
-            let item_doc = db.get_item_documentation(LookupItemId::ModuleItem(*item));
-            item_documentation.insert(LookupItemId::ModuleItem(*item), item_doc);
-
-            if let ModuleItemId::Trait(trait_id) = *item {
-                let trait_items_names = db.trait_required_item_names(trait_id).unwrap();
-
-                for trait_item_name in trait_items_names.into_iter() {
-                    let trait_item_id = db
-                        .trait_item_by_name(trait_id, trait_item_name)
-                        .unwrap()
-                        .unwrap();
-
-                    let doc = db.get_item_documentation(LookupItemId::TraitItem(trait_item_id));
-                    item_documentation.insert(LookupItemId::TraitItem(trait_item_id), doc);
-                }
-            }
-        }
-    }
-
-    for (item_id, doc) in item_documentation.iter() {
-        let name = match item_id {
-            LookupItemId::ModuleItem(item_id) => item_id.name(db),
-            LookupItemId::TraitItem(item_id) => item_id.name(db),
-            LookupItemId::ImplItem(item_id) => item_id.name(db),
-        };
-        println!("{:?}: {:?} -> {:?}", name, item_id, doc);
-    }
+    print_module(&crate_.root_module);
 
     Ok(())
 }
 
-fn get_project_config(metadata: &Metadata, package_metadata: &PackageMetadata) -> ProjectConfig {
-    let compilation_unit_metadata =
-        package_lib_compilation_unit(metadata, package_metadata.id.clone())
-            .expect("failed to find compilation unit for package");
-    let corelib = get_corelib(compilation_unit_metadata);
-    let dependencies = get_dependencies(compilation_unit_metadata);
-    let crates_config = get_crates_config(metadata, compilation_unit_metadata);
+fn print_module(module: &types::Module) {
+    println!("Module: {}", module.full_path);
+    println!(
+        "Submodules      : {:?}",
+        module
+            .submodules
+            .iter()
+            .map(|x| &x.full_path)
+            .collect::<Vec<_>>()
+    );
+    print_names!("Constants       ", module.constants);
+    print_names!("Uses            ", module.uses);
+    print_names!("Free Functions  ", module.free_functions);
+    print_names!("Structs         ", module.structs);
+    print_names!("Enums           ", module.enums);
+    print_names!("Type Aliases    ", module.type_aliases);
+    print_names!("Impl Aliases    ", module.impl_aliases);
+    print_names!("Traits          ", module.traits);
+    print_names!("Impls           ", module.impls);
+    print_names!("Extern Types    ", module.extern_types);
+    print_names!("Extern Functions", module.extern_functions);
 
-    ProjectConfig {
-        base_path: package_metadata.root.clone().into(),
-        corelib: Some(Directory::Real(corelib.source_root().into())),
-        content: ProjectConfigContent {
-            crate_roots: dependencies,
-            crates_config,
-        },
+    for submodule in &module.submodules {
+        println!();
+        print_module(submodule);
     }
 }
-
-fn package_lib_compilation_unit(
-    metadata: &Metadata,
-    package_id: PackageId,
-) -> Option<&CompilationUnitMetadata> {
-    metadata
-        .compilation_units
-        .iter()
-        .find(|m| m.package == package_id && m.target.kind == LIB_TARGET_KIND)
-}
-
-fn get_corelib(
-    compilation_unit_metadata: &CompilationUnitMetadata,
-) -> &CompilationUnitComponentMetadata {
-    compilation_unit_metadata
-        .components
-        .iter()
-        .find(|du| du.name == CORELIB_CRATE_NAME)
-        .expect("Corelib could not be found")
-}
-
-fn get_dependencies(
-    compilation_unit_metadata: &CompilationUnitMetadata,
-) -> OrderedHashMap<SmolStr, PathBuf> {
-    compilation_unit_metadata
-        .components
-        .iter()
-        .filter(|du| du.name != CORELIB_CRATE_NAME)
-        .map(|cu| {
-            (
-                cu.name.to_smolstr(),
-                cu.source_root().to_owned().into_std_path_buf(),
-            )
-        })
-        .collect()
-}
-
-fn get_crates_config(
-    metadata: &Metadata,
-    compilation_unit_metadata: &CompilationUnitMetadata,
-) -> AllCratesConfig {
-    let crates_config: OrderedHashMap<SmolStr, CrateSettings> = compilation_unit_metadata
-        .components
-        .iter()
-        .map(|component| {
-            let pkg = metadata.get_package(&component.package).unwrap_or_else(|| {
-                panic!(
-                    "failed to find = {} package",
-                    &component.package.to_string()
-                )
-            });
-            (
-                SmolStr::from(&component.name),
-                get_crate_settings_for_package(
-                    pkg,
-                    component.cfg.as_ref().map(|cfg_vec| build_cfg_set(cfg_vec)),
-                ),
-            )
-        })
-        .collect();
-
-    AllCratesConfig {
-        override_map: crates_config,
-        ..Default::default()
-    }
-}
-
-fn get_crate_settings_for_package(
-    package: &PackageMetadata,
-    cfg_set: Option<CfgSet>,
-) -> CrateSettings {
-    let edition = package
-        .edition
-        .clone()
-        .map_or(Edition::default(), |edition| {
-            let edition_value = serde_json::Value::String(edition);
-            serde_json::from_value(edition_value).unwrap()
-        });
-
-    let experimental_features = ExperimentalFeaturesConfig {
-        negative_impls: package
-            .experimental_features
-            .contains(&String::from("negative_impls")),
-        coupons: package
-            .experimental_features
-            .contains(&String::from("coupons")),
-    };
-
-    CrateSettings {
-        edition,
-        cfg_set,
-        experimental_features,
-    }
-}
-
-fn build_cfg_set(cfg: &[scarb_metadata::Cfg]) -> CfgSet {
-    CfgSet::from_iter(cfg.iter().map(|cfg| {
-        serde_json::to_value(cfg)
-            .and_then(serde_json::from_value::<Cfg>)
-            .expect("Cairo's `Cfg` must serialize identically as Scarb Metadata's `Cfg`.")
-    }))
-}
-
-const LIB_TARGET_KIND: &str = "lib";
-const CORELIB_CRATE_NAME: &str = "core";
---
+++ extensions/scarb-doc/src/types.rs
@@ -0,0 +1,354 @@
+use cairo_lang_defs::db::DefsGroup;
+use cairo_lang_defs::ids::{
+    ConstantId, EnumId, ExternFunctionId, ExternTypeId, FreeFunctionId, ImplAliasId, ImplDefId,
+    LookupItemId, ModuleId, ModuleItemId, ModuleTypeAliasId, NamedLanguageElementId, StructId,
+    TopLevelLanguageElementId, TraitId, UseId,
+};
+use cairo_lang_filesystem::ids::CrateId;
+use cairo_lang_syntax::node::{ast, TypedSyntaxNode};
+
+#[derive(Clone, Debug)]
+pub struct Crate {
+    pub root_module: Module,
+}
+
+impl Crate {
+    pub fn new(db: &dyn DefsGroup, crate_id: CrateId) -> Self {
+        Self {
+            root_module: Module::new(db, ModuleId::CrateRoot(crate_id)),
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct Module {
+    pub module_id: ModuleId,
+    pub full_path: String,
+
+    pub submodules: Vec<Module>,
+    pub constants: Vec<Constant>,
+    pub uses: Vec<Use>,
+    pub free_functions: Vec<FreeFunction>,
+    pub structs: Vec<Struct>,
+    pub enums: Vec<Enum>,
+    pub type_aliases: Vec<TypeAlias>,
+    pub impl_aliases: Vec<ImplAlias>,
+    pub traits: Vec<Trait>,
+    pub impls: Vec<Impl>,
+    pub extern_types: Vec<ExternType>,
+    pub extern_functions: Vec<ExternFunction>,
+}
+
+impl Module {
+    pub fn new(db: &dyn DefsGroup, module_id: ModuleId) -> Self {
+        let module_constants = db.module_constants(module_id).unwrap();
+        let constants = module_constants
+            .iter()
+            .map(|(id, node)| Constant::new(db, *id, node))
+            .collect();
+
+        let module_uses = db.module_uses(module_id).unwrap();
+        let uses = module_uses
+            .iter()
+            .map(|(id, node)| Use::new(db, *id, node))
+            .collect();
+
+        let module_free_functions = db.module_free_functions(module_id).unwrap();
+        let free_functions = module_free_functions
+            .iter()
+            .map(|(id, node)| FreeFunction::new(db, *id, node))
+            .collect();
+
+        let module_structs = db.module_structs(module_id).unwrap();
+        let structs = module_structs
+            .iter()
+            .map(|(id, node)| Struct::new(db, *id, node))
+            .collect();
+
+        let module_enums = db.module_enums(module_id).unwrap();
+        let enums = module_enums
+            .iter()
+            .map(|(id, node)| Enum::new(db, *id, node))
+            .collect();
+
+        let module_type_aliases = db.module_type_aliases(module_id).unwrap();
+        let type_aliases = module_type_aliases
+            .iter()
+            .map(|(id, node)| TypeAlias::new(db, *id, node))
+            .collect();
+
+        let module_impl_aliases = db.module_impl_aliases(module_id).unwrap();
+        let impl_aliases = module_impl_aliases
+            .iter()
+            .map(|(id, node)| ImplAlias::new(db, *id, node))
+            .collect();
+
+        let module_traits = db.module_traits(module_id).unwrap();
+        let traits = module_traits
+            .iter()
+            .map(|(id, node)| Trait::new(db, *id, node))
+            .collect();
+
+        let module_impls = db.module_impls(module_id).unwrap();
+        let impls = module_impls
+            .iter()
+            .map(|(id, node)| Impl::new(db, *id, node))
+            .collect();
+
+        let module_extern_types = db.module_extern_types(module_id).unwrap();
+        let extern_types = module_extern_types
+            .iter()
+            .map(|(id, node)| ExternType::new(db, *id, node))
+            .collect();
+
+        let module_extern_functions = db.module_extern_functions(module_id).unwrap();
+        let extern_functions = module_extern_functions
+            .iter()
+            .map(|(id, node)| ExternFunction::new(db, *id, node))
+            .collect();
+
+        let module_submodules = db.module_submodules(module_id).unwrap();
+        let submodules = module_submodules
+            .iter()
+            .map(|(id, _node)| Self::new(db, ModuleId::Submodule(*id)))
+            .collect();
+
+        Self {
+            module_id,
+            full_path: module_id.full_path(db),
+            submodules,
+            constants,
+            uses,
+            free_functions,
+            structs,
+            enums,
+            type_aliases,
+            impl_aliases,
+            traits,
+            impls,
+            extern_types,
+            extern_functions,
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct ItemData {
+    pub name: String,
+    pub full_path: String,
+
+    pub doc: Option<String>,
+    pub definition: String,
+
+    pub text: String,
+}
+
+impl ItemData {
+    pub fn new(db: &dyn DefsGroup, id: ModuleItemId, node: &impl TypedSyntaxNode) -> Self {
+        Self {
+            name: id.name(db).into(),
+            full_path: id.full_path(db),
+            doc: db.get_item_documentation(LookupItemId::ModuleItem(id)),
+            definition: db.get_item_definition(LookupItemId::ModuleItem(id)),
+            text: node.as_syntax_node().get_text_without_trivia(db.upcast()),
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct Constant {
+    pub id: ConstantId,
+    pub node: ast::ItemConstantPtr,
+
+    pub item_data: ItemData,
+}
+
+impl Constant {
+    pub fn new(db: &dyn DefsGroup, id: ConstantId, node: &ast::ItemConstant) -> Self {
+        Self {
+            id,
+            node: node.stable_ptr(),
+            item_data: ItemData::new(db, ModuleItemId::Constant(id), node),
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct Use {
+    pub id: UseId,
+    pub node: ast::UsePathLeafPtr,
+
+    pub item_data: ItemData,
+}
+
+impl Use {
+    pub fn new(db: &dyn DefsGroup, id: UseId, node: &ast::UsePathLeaf) -> Self {
+        Self {
+            id,
+            node: node.stable_ptr(),
+            item_data: ItemData::new(db, ModuleItemId::Use(id), node),
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct FreeFunction {
+    pub id: FreeFunctionId,
+    pub node: ast::FunctionWithBodyPtr,
+
+    pub item_data: ItemData,
+}
+
+impl FreeFunction {
+    pub fn new(db: &dyn DefsGroup, id: FreeFunctionId, node: &ast::FunctionWithBody) -> Self {
+        Self {
+            id,
+            node: node.stable_ptr(),
+            item_data: ItemData::new(db, ModuleItemId::FreeFunction(id), node),
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct Struct {
+    pub id: StructId,
+    pub node: ast::ItemStructPtr,
+
+    pub item_data: ItemData,
+}
+
+impl Struct {
+    pub fn new(db: &dyn DefsGroup, id: StructId, node: &ast::ItemStruct) -> Self {
+        Self {
+            id,
+            node: node.stable_ptr(),
+            item_data: ItemData::new(db, ModuleItemId::Struct(id), node),
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct Enum {
+    pub id: EnumId,
+    pub node: ast::ItemEnumPtr,
+
+    pub item_data: ItemData,
+}
+
+impl Enum {
+    pub fn new(db: &dyn DefsGroup, id: EnumId, node: &ast::ItemEnum) -> Self {
+        Self {
+            id,
+            node: node.stable_ptr(),
+            item_data: ItemData::new(db, ModuleItemId::Enum(id), node),
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct TypeAlias {
+    pub id: ModuleTypeAliasId,
+    pub node: ast::ItemTypeAliasPtr,
+
+    pub item_data: ItemData,
+}
+
+impl TypeAlias {
+    pub fn new(db: &dyn DefsGroup, id: ModuleTypeAliasId, node: &ast::ItemTypeAlias) -> Self {
+        Self {
+            id,
+            node: node.stable_ptr(),
+            item_data: ItemData::new(db, ModuleItemId::TypeAlias(id), node),
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct ImplAlias {
+    pub id: ImplAliasId,
+    pub node: ast::ItemImplAliasPtr,
+
+    pub item_data: ItemData,
+}
+
+impl ImplAlias {
+    pub fn new(db: &dyn DefsGroup, id: ImplAliasId, node: &ast::ItemImplAlias) -> Self {
+        Self {
+            id,
+            node: node.stable_ptr(),
+            item_data: ItemData::new(db, ModuleItemId::ImplAlias(id), node),
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct Trait {
+    pub id: TraitId,
+    pub node: ast::ItemTraitPtr,
+
+    pub item_data: ItemData,
+}
+
+impl Trait {
+    pub fn new(db: &dyn DefsGroup, id: TraitId, node: &ast::ItemTrait) -> Self {
+        Self {
+            id,
+            node: node.stable_ptr(),
+            item_data: ItemData::new(db, ModuleItemId::Trait(id), node),
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct Impl {
+    pub id: ImplDefId,
+    pub node: ast::ItemImplPtr,
+
+    pub item_data: ItemData,
+}
+
+impl Impl {
+    pub fn new(db: &dyn DefsGroup, id: ImplDefId, node: &ast::ItemImpl) -> Self {
+        Self {
+            id,
+            node: node.stable_ptr(),
+            item_data: ItemData::new(db, ModuleItemId::Impl(id), node),
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct ExternType {
+    pub id: ExternTypeId,
+    pub node: ast::ItemExternTypePtr,
+
+    pub item_data: ItemData,
+}
+
+impl ExternType {
+    pub fn new(db: &dyn DefsGroup, id: ExternTypeId, node: &ast::ItemExternType) -> Self {
+        Self {
+            id,
+            node: node.stable_ptr(),
+            item_data: ItemData::new(db, ModuleItemId::ExternType(id), node),
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct ExternFunction {
+    pub id: ExternFunctionId,
+    pub node: ast::ItemExternFunctionPtr,
+
+    pub item_data: ItemData,
+}
+
+impl ExternFunction {
+    pub fn new(db: &dyn DefsGroup, id: ExternFunctionId, node: &ast::ItemExternFunction) -> Self {
+        Self {
+            id,
+            node: node.stable_ptr(),
+            item_data: ItemData::new(db, ModuleItemId::ExternFunction(id), node),
+        }
+    }
+}
