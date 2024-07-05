50a694ea0770f067f9ce7275d84ea0a12300a064
Maksim Zdobnikau <maksim.zdobnikau@swmansion.com>
2024-07-05 19:47:56 +0200

    Add check to `prepare_manifest_for_publish`

--- scarb/src/core/publishing/manifest_normalization.rs
+++ scarb/src/core/publishing/manifest_normalization.rs
@@ -1,6 +1,6 @@
 use std::collections::BTreeMap;
 
-use anyhow::{bail, Result};
+use anyhow::{bail, ensure, Result};
 use camino::Utf8PathBuf;
 use indoc::formatdoc;
 
@@ -14,6 +14,16 @@ use crate::{
 };
 
 pub fn prepare_manifest_for_publish(pkg: &Package) -> Result<TomlManifest> {
+    ensure!(
+        pkg.is_publishable(),
+        formatdoc! {
+            r#"
+                publishing disabled for package {package_name}
+                help: set `publish = true` in package manifest
+            "#,
+            package_name = pkg.id.name,
+        }
+    );
     let package = Some(generate_package(pkg));
 
     let dependencies = Some(generate_dependencies(
