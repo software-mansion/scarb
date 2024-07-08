d5c6b54372e7b6810e69df08377f97dd1406565f
Piotr Magiera <piotrmagiera150@gmail.com>
2024-07-08 13:42:49 +0200

    JSON output for scarb-doc

--- extensions/scarb-doc/src/main.rs
+++ extensions/scarb-doc/src/main.rs
@@ -12,6 +12,13 @@ use scarb_doc::generate_language_elements_tree_for_package;
 struct Args {
     #[command(flatten)]
     packages_filter: PackagesFilter,
+
+    /// Print information collected from packages to stdout in JSON format instead of generating a
+    /// documentation.
+    /// This feature may be useful if you want to generate documentation files by yourself.
+    /// The precise output structure is not guaranteed to be stable.
+    #[arg(long)]
+    unstable_json_output: bool,
 }
 
 fn main() -> Result<()> {
@@ -20,10 +27,27 @@ fn main() -> Result<()> {
     let metadata = MetadataCommand::new().inherit_stderr().exec()?;
     let metadata_for_packages = args.packages_filter.match_many(&metadata)?;
 
+    let mut json_output = serde_json::Map::new();
+
     for package_metadata in metadata_for_packages {
         let project_config = get_project_config(&metadata, &package_metadata);
-        let _crate_ =
-            generate_language_elements_tree_for_package(package_metadata.name, project_config)?;
+        let crate_ = generate_language_elements_tree_for_package(
+            package_metadata.name.clone(),
+            project_config,
+        )?;
+
+        json_output.insert(
+            package_metadata.name,
+            serde_json::to_value(crate_).expect("Failed to serialize information about a crate"),
+        );
+    }
+
+    if args.unstable_json_output {
+        println!(
+            "{}",
+            serde_json::to_string_pretty(&json_output)
+                .expect("Failed to serialize information about crates")
+        );
     }
 
     Ok(())
