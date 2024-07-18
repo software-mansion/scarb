aa7866f5ad2715ead7bc4a55e01d0bc5a2c08498
Piotr Magiera <56825108+piotmag769@users.noreply.github.com>
2024-07-18 15:16:36 +0200

    Extract package metadata info + refactor (#1460)
    commit-id:b9eb7559
    
    ---
    
    **Stack**:
    - #1464
    - #1463
    - #1462
    - #1461
    - #1460 ⬅
    
    
    ⚠️ *Part of a stack created by [spr](https://github.com/ejoffe/spr). Do
    not merge manually using the UI - doing so may have unexpected results.*

--- extensions/scarb-doc/src/versioned_json_output.rs
+++ extensions/scarb-doc/src/versioned_json_output.rs
@@ -1,22 +1,38 @@
-use crate::types::Crate;
+use crate::PackageInformation;
+use anyhow::{Context, Result};
+use camino::Utf8Path;
 use serde::Serialize;
-use std::collections::BTreeMap;
-
-type PackageName = String;
+use std::fs;
 
 const FORMAT_VERSION: u8 = 1;
+const JSON_OUTPUT_FILENAME: &str = "output.json";
 
 #[derive(Serialize)]
 pub struct VersionedJsonOutput {
     format_version: u8,
-    pub package_information_map: BTreeMap<PackageName, Crate>,
+    packages_information: Vec<PackageInformation>,
 }
 
 impl VersionedJsonOutput {
-    pub fn new(package_information_map: BTreeMap<PackageName, Crate>) -> Self {
+    pub fn new(packages_information: Vec<PackageInformation>) -> Self {
         Self {
             format_version: FORMAT_VERSION,
-            package_information_map,
+            packages_information,
         }
+    }
+
+    pub fn save_to_file(&self, output_dir: &Utf8Path) -> Result<()> {
+        fs::create_dir_all(output_dir)
+            .context("failed to create output directory for scarb doc")?;
+
+        let output_path = output_dir.join(JSON_OUTPUT_FILENAME);
+
+        let output = serde_json::to_string_pretty(&self)
+            .expect("failed to serialize information about crates")
+            + "\n";
+
+        fs::write(output_path, output)?;
+
+        Ok(())
     }
 }
