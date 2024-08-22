295935d87b9731ea05839f7901f735bebf3d7060
maciektr <maciej.tratnowiecki@swmansion.com>
2024-08-22 02:50:47 -0700

    Update Cairo, move scarb-doc to rust-analyzer-salsa (#1531)

--- scarb/src/compiler/compilers/test.rs
+++ scarb/src/compiler/compilers/test.rs
@@ -46,6 +46,7 @@ impl Compiler for TestCompiler {
                 add_statements_functions: unit
                     .compiler_config
                     .unstable_add_statements_functions_debug_info,
+                add_statements_code_locations: false,
             };
             let allow_warnings = unit.compiler_config.allow_warnings;
             compile_test_prepared_db(db, config, main_crate_ids, test_crate_ids, allow_warnings)?
