243ea2b2c9dbc8dfe379139159008c71dd30cae7
Maksim Zdobnikau <maksim.zdobnikau@swmansion.com>
2024-09-03 12:50:16 +0200

    Format

--- scarb/tests/flock.rs
+++ scarb/tests/flock.rs
@@ -25,11 +25,10 @@ fn locking_build_artifacts() {
 
     thread::scope(|s| {
         let ws = scarb::ops::read_workspace(config.manifest_path(), &config).unwrap();
-        let lock = ws.target_dir().child(config.profile().to_string()).create_rw(
-            "hello.sierra.json",
-            "artifact",
-            &config,
-        );
+        let lock = ws
+            .target_dir()
+            .child(config.profile().to_string())
+            .create_rw("hello.sierra.json", "artifact", &config);
         let barrier = Arc::new(Barrier::new(2));
 
         s.spawn({
