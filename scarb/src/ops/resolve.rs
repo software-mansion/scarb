8a9e63d31549e8059d7638b9148dca8760b0020e
Maksim Zdobnikau <maksim.zdobnikau@swmansion.com>
2024-10-16 10:37:22 +0200

    Remove unnecessary `unwrap()`

--- scarb/src/ops/resolve.rs
+++ scarb/src/ops/resolve.rs
@@ -381,8 +381,7 @@ fn cairo_compilation_unit_for_target(
                 no_test_cfg_set.unwrap_or(CfgSet::new()),
                 &member.manifest.features,
                 enabled_features,
-            )
-            .unwrap(),
+            )?,
         )?);
 
         // Set test package as main package for this compilation unit.
