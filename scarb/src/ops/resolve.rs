2058b1cfdb5dfcb7618505c3b42041af2aae8957
Maksim Zdobnikau <43750648+DelevoXDG@users.noreply.github.com>
2024-10-16 12:19:44 +0300

    Enable features for main package during integration tests compilation (#1646)
    Closes #1637

--- scarb/src/ops/resolve.rs
+++ scarb/src/ops/resolve.rs
@@ -377,7 +377,11 @@ fn cairo_compilation_unit_for_target(
         components.push(CompilationUnitComponent::try_new(
             member.clone(),
             vec![target],
-            no_test_cfg_set,
+            get_cfg_with_features(
+                no_test_cfg_set.unwrap_or(CfgSet::new()),
+                &member.manifest.features,
+                enabled_features,
+            )?,
         )?);
 
         // Set test package as main package for this compilation unit.
