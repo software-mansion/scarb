cf75e277068f3305270c548e1e3d90230da74c2a
Piotr Figiela <77412592+Draggu@users.noreply.github.com>
2024-10-23 18:26:14 +0200

    Add `ExpandAttribute` handler
    commit-id:ab75be72

--- scarb/src/ops/proc_macro_server/methods/mod.rs
+++ scarb/src/ops/proc_macro_server/methods/mod.rs
@@ -6,6 +6,7 @@ use scarb_proc_macro_server_types::methods::Method;
 use crate::compiler::plugin::proc_macro::ProcMacroHost;
 
 pub mod defined_macros;
+pub mod expand_attribute;
 
 pub trait Handler: Method {
     fn handle(proc_macro_host: Arc<ProcMacroHost>, params: Self::Params) -> Result<Self::Response>;
