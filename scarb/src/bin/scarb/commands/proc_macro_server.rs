9d619fd46f5a46842631bb76d46202ac1f088b2e
Piotr Figiela <77412592+Draggu@users.noreply.github.com>
2024-10-23 17:46:04 +0200

    Proc macro server: Load `ProcMacroHost`

--- scarb/src/bin/scarb/commands/proc_macro_server.rs
+++ scarb/src/bin/scarb/commands/proc_macro_server.rs
@@ -1,7 +1,52 @@
 use anyhow::Result;
-use scarb::core::Config;
+use scarb::{
+    compiler::{plugin::proc_macro::ProcMacroHost, CairoCompilationUnit, CompilationUnit},
+    core::{Config, Workspace},
+    ops::{self, FeaturesOpts, FeaturesSelector},
+};
 
 #[tracing::instrument(skip_all, level = "info")]
-pub fn run(_config: &Config) -> Result<()> {
-    unimplemented!()
+pub fn run(config: &mut Config) -> Result<()> {
+    let ws = ops::read_workspace(config.manifest_path(), config)?;
+    let resolve = ops::resolve_workspace(&ws)?;
+    let compilation_units = ops::generate_compilation_units(
+        &resolve,
+        &FeaturesOpts {
+            features: FeaturesSelector::AllFeatures,
+            no_default_features: false,
+        },
+        &ws,
+    )?;
+
+    for unit in &compilation_units {
+        if let CompilationUnit::ProcMacro(_) = unit {
+            ops::compile_unit(unit.clone(), &ws)?;
+        }
+    }
+
+    let mut proc_macros = ProcMacroHost::default();
+
+    for unit in compilation_units {
+        if let CompilationUnit::Cairo(unit) = unit {
+            load_plugins(unit, &ws, &mut proc_macros)?;
+        }
+    }
+
+    ops::start_proc_macro_server(proc_macros)
+}
+
+fn load_plugins(
+    unit: CairoCompilationUnit,
+    ws: &Workspace<'_>,
+    proc_macros: &mut ProcMacroHost,
+) -> Result<()> {
+    for plugin_info in unit
+        .cairo_plugins
+        .into_iter()
+        .filter(|plugin_info| !plugin_info.builtin)
+    {
+        proc_macros.register(plugin_info.package, ws.config())?;
+    }
+
+    Ok(())
 }
---
+++ scarb/src/ops/proc_macro_server/mod.rs
@@ -0,0 +1,6 @@
+use crate::compiler::plugin::proc_macro::ProcMacroHost;
+use anyhow::Result;
+
+pub fn start_proc_macro_server(proc_macros: ProcMacroHost) -> Result<()> {
+    unimplemented!()
+}
