223ad0628c1dc023bab987c75fd98503af7394dc
Mateusz Kowalski <wawel37@gmail.com>
2025-01-08 14:33:07 +0100

    Moved cli frontend of the cairo linter to scarb command

---
+++ scarb/src/bin/scarb/commands/lint.rs
@@ -0,0 +1,24 @@
+use scarb::ops::{self, LintOptions};
+
+use crate::args::LintArgs;
+use anyhow::Result;
+use scarb::core::Config;
+
+#[tracing::instrument(skip_all, level = "info")]
+pub fn run(args: LintArgs, config: &Config) -> Result<()> {
+    let ws = ops::read_workspace(config.manifest_path(), config)?;
+    let packages = args
+        .packages_filter
+        .match_many(&ws)?
+        .into_iter()
+        .collect::<Vec<_>>();
+
+    ops::lint(
+        LintOptions {
+            packages,
+            test: args.test,
+            fix: args.fix,
+        },
+        &ws,
+    )
+}
