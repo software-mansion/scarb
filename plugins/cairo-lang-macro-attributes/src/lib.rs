0bd518e87f2b1a96b847eeea823bd3a1d53257fb
maciektr <maciej.tratnowiecki@swmansion.com>
2025-01-17 17:07:48 +0100

    Allow renaming macro attributes (#1891)
    Signed-off-by: maciektr <mtratnowiecki@gmail.com>
    Co-authored-by: Rémy Baranx <remy.baranx@gmail.com>
    Co-authored-by: Maksim Zdobnikau <43750648+DelevoXDG@users.noreply.github.com>

--- plugins/cairo-lang-macro-attributes/src/lib.rs
+++ plugins/cairo-lang-macro-attributes/src/lib.rs
@@ -2,7 +2,10 @@ use proc_macro::TokenStream;
 use quote::{quote, ToTokens};
 use scarb_stable_hash::short_hash;
 use syn::spanned::Spanned;
-use syn::{parse_macro_input, Expr, ItemFn, LitStr, Meta};
+use syn::{
+    parse::Parse, parse::ParseStream, parse_macro_input, Expr, Ident, ItemFn, LitStr, Meta, Result,
+    Token,
+};
 
 /// Constructs the attribute macro implementation.
 ///
@@ -10,9 +13,10 @@ use syn::{parse_macro_input, Expr, ItemFn, LitStr, Meta};
 ///
 /// Note, that this macro can be used multiple times, to define multiple independent attribute macros.
 #[proc_macro_attribute]
-pub fn attribute_macro(_args: TokenStream, input: TokenStream) -> TokenStream {
+pub fn attribute_macro(args: TokenStream, input: TokenStream) -> TokenStream {
     macro_helper(
         input,
+        parse_macro_input!(args as AttributeArgs),
         quote!(::cairo_lang_macro::ExpansionKind::Attr),
         quote!(::cairo_lang_macro::ExpansionFunc::Attr),
     )
@@ -24,9 +28,18 @@ pub fn attribute_macro(_args: TokenStream, input: TokenStream) -> TokenStream {
 ///
 /// Note, that this macro can be used multiple times, to define multiple independent attribute macros.
 #[proc_macro_attribute]
-pub fn inline_macro(_args: TokenStream, input: TokenStream) -> TokenStream {
+pub fn inline_macro(args: TokenStream, input: TokenStream) -> TokenStream {
+    // Emit compilation error if `parent` argument is used.
+    let attribute_args = parse_macro_input!(args as AttributeArgs);
+    if let Some(path) = attribute_args.parent_module_path {
+        return syn::Error::new(path.span(), "inline macro cannot use `parent` argument")
+            .to_compile_error()
+            .into();
+    }
+    // Otherwise, proceed with the macro expansion.
     macro_helper(
         input,
+        Default::default(),
         quote!(::cairo_lang_macro::ExpansionKind::Inline),
         quote!(::cairo_lang_macro::ExpansionFunc::Other),
     )
@@ -38,18 +51,35 @@ pub fn inline_macro(_args: TokenStream, input: TokenStream) -> TokenStream {
 ///
 /// Note, that this macro can be used multiple times, to define multiple independent attribute macros.
 #[proc_macro_attribute]
-pub fn derive_macro(_args: TokenStream, input: TokenStream) -> TokenStream {
+pub fn derive_macro(args: TokenStream, input: TokenStream) -> TokenStream {
     macro_helper(
         input,
+        parse_macro_input!(args as AttributeArgs),
         quote!(::cairo_lang_macro::ExpansionKind::Derive),
         quote!(::cairo_lang_macro::ExpansionFunc::Other),
     )
 }
 
-fn macro_helper(input: TokenStream, kind: impl ToTokens, func: impl ToTokens) -> TokenStream {
+fn macro_helper(
+    input: TokenStream,
+    args: AttributeArgs,
+    kind: impl ToTokens,
+    func: impl ToTokens,
+) -> TokenStream {
     let item: ItemFn = parse_macro_input!(input as ItemFn);
 
     let original_item_name = item.sig.ident.to_string();
+    let expansion_name = if let Some(path) = args.parent_module_path {
+        let value = path.value();
+        if !is_valid_path(&value) {
+            return syn::Error::new(path.span(), "`parent` argument is not a valid path")
+                .to_compile_error()
+                .into();
+        }
+        format!("{}::{}", value, original_item_name)
+    } else {
+        original_item_name
+    };
     let doc = item
         .attrs
         .iter()
@@ -74,7 +104,7 @@ fn macro_helper(input: TokenStream, kind: impl ToTokens, func: impl ToTokens) ->
         item_name.to_string().to_uppercase()
     );
 
-    let callback_link = syn::Ident::new(callback_link.as_str(), item.span());
+    let callback_link = Ident::new(callback_link.as_str(), item.span());
 
     let expanded = quote! {
         #item
@@ -83,13 +113,62 @@ fn macro_helper(input: TokenStream, kind: impl ToTokens, func: impl ToTokens) ->
         #[linkme(crate = ::cairo_lang_macro::linkme)]
         static #callback_link: ::cairo_lang_macro::ExpansionDefinition =
             ::cairo_lang_macro::ExpansionDefinition{
-                name: #original_item_name,
+                name: #expansion_name,
                 doc: #doc,
                 kind: #kind,
                 fun: #func(#item_name),
             };
     };
     TokenStream::from(expanded)
+}
+
+#[derive(Default)]
+struct AttributeArgs {
+    parent_module_path: Option<LitStr>,
+}
+
+impl Parse for AttributeArgs {
+    fn parse(input: ParseStream) -> Result<Self> {
+        if input.is_empty() {
+            return Ok(Self {
+                parent_module_path: None,
+            });
+        }
+        let parent_identifier: Ident = input.parse()?;
+        if parent_identifier != "parent" {
+            return Err(input.error("only `parent` argument is supported"));
+        }
+        let _eq_token: Token![=] = input.parse()?;
+        let parent_module_path: LitStr = input.parse()?;
+        Ok(Self {
+            parent_module_path: Some(parent_module_path),
+        })
+    }
+}
+
+fn is_valid_path(path: &str) -> bool {
+    let mut chars = path.chars().peekable();
+    let mut last_was_colon = false;
+    while let Some(c) = chars.next() {
+        if c.is_alphanumeric() || c == '_' {
+            last_was_colon = false;
+        } else if c == ':' {
+            if last_was_colon {
+                // If the last character was also a colon, continue
+                last_was_colon = false;
+            } else {
+                // If the next character is not a colon, it's an error
+                if chars.peek() != Some(&':') {
+                    return false;
+                }
+                last_was_colon = true;
+            }
+        } else {
+            return false;
+        }
+    }
+    // If the loop ends with a colon flag still true, it means the string ended with a single colon.
+    !last_was_colon
 }
 
 /// Constructs the post-processing callback.
@@ -123,7 +202,7 @@ pub fn post_process(_args: TokenStream, input: TokenStream) -> TokenStream {
         "POST_PROCESS_DESERIALIZE_{}",
         item_name.to_string().to_uppercase()
     );
-    let callback_link = syn::Ident::new(callback_link.as_str(), item.span());
+    let callback_link = Ident::new(callback_link.as_str(), item.span());
 
     let expanded = quote! {
         #item
@@ -140,7 +219,7 @@ pub fn post_process(_args: TokenStream, input: TokenStream) -> TokenStream {
 fn hide_name(mut item: ItemFn) -> ItemFn {
     let id = short_hash(item.sig.ident.to_string());
     let item_name = format!("{}_{}", item.sig.ident, id);
-    item.sig.ident = syn::Ident::new(item_name.as_str(), item.sig.ident.span());
+    item.sig.ident = Ident::new(item_name.as_str(), item.sig.ident.span());
     item
 }
 
@@ -150,9 +229,9 @@ const EXEC_ATTR_PREFIX: &str = "__exec_attr_";
 pub fn executable_attribute(input: TokenStream) -> TokenStream {
     let input: LitStr = parse_macro_input!(input as LitStr);
     let callback_link = format!("EXEC_ATTR_DESERIALIZE{}", input.value().to_uppercase());
-    let callback_link = syn::Ident::new(callback_link.as_str(), input.span());
+    let callback_link = Ident::new(callback_link.as_str(), input.span());
     let item_name = format!("{EXEC_ATTR_PREFIX}{}", input.value());
-    let org_name = syn::Ident::new(item_name.as_str(), input.span());
+    let org_name = Ident::new(item_name.as_str(), input.span());
     let expanded = quote! {
         fn #org_name() {
             // No op to ensure no function with the same name is created.
