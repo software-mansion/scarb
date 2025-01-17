4c7feb51bed138465d2b2f34cb40e13ebe6bd780
Maciej Trątnowiecki <maciej.tratnowiecki@swmansion.com>
2025-01-17 12:28:47 +0100

    Run cairo-lang-macro-attributes tests with cargo instead of nextest

--- plugins/cairo-lang-macro/tests/args/args_01.rs
+++ plugins/cairo-lang-macro/tests/args/args_01.rs
@@ -1,6 +1,6 @@
 use cairo_lang_macro::{attribute_macro, ProcMacroResult, TokenStream};
 
-#[attribute_macro(path = "some::path")]
+#[attribute_macro(unsupported_key = "some::path")]
 fn t1(_a: TokenStream, _b: TokenStream) -> ProcMacroResult {
     ProcMacroResult::new(TokenStream::empty())
 }
