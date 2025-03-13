e945f6aece25ad47a3b796a0597176ef2d178d74
Maciej Trątnowiecki <maciej.tratnowiecki@swmansion.com>
2025-03-13 20:43:57 +0100

    Proc macro: move common struct to top-level proc macro module

--- scarb/src/compiler/plugin/proc_macro/mod.rs
+++ scarb/src/compiler/plugin/proc_macro/mod.rs
@@ -1,6 +1,12 @@
+pub mod compilation;
 mod instance;
+mod repository;
+mod shared_library_provider;
 pub mod v1;
 pub mod v2;
 
+pub use compilation::{check_unit, compile_unit, fetch_crate};
 pub use instance::*;
+pub use repository::*;
+pub use shared_library_provider::SharedLibraryProvider;
 pub use v2::*;
