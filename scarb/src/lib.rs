0cb3cfa79c78ccf44a94be05f0fc5f082aa5eb51
Maksim Zdobnikau <43750648+DelevoXDG@users.noreply.github.com>
2025-03-21 12:47:35 +0300

    Include `Cargo.lock` when packaging proc macros  (#2092)
    Closes [#2053](https://github.com/software-mansion/scarb/issues/2053)

--- scarb/src/lib.rs
+++ scarb/src/lib.rs
@@ -42,5 +42,5 @@ pub const TEST_PLUGIN_NAME: &str = "cairo_test";
 pub const TEST_ASSERTS_PLUGIN_NAME: &str = "assert_macros";
 pub const CAIRO_RUN_PLUGIN_NAME: &str = "cairo_run";
 pub const CARGO_MANIFEST_FILE_NAME: &str = "Cargo.toml";
-pub const CARGO_LOCK_FILE_NAME: &str = "Cargo.lock";
+pub const CARGO_LOCKFILE_FILE_NAME: &str = "Cargo.lock";
 pub const EXECUTABLE_PLUGIN_NAME: &str = "cairo_execute";
