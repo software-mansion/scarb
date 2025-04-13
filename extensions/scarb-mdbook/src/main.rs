79d96f3e5e5488928588b7fcd1eb226969f86942
Maciej Trątnowiecki <maciej.tratnowiecki@swmansion.com>
2025-04-13 17:36:56 +0200

    fixup! Patch highlighter for Cairo when building with scarb-mdbook

--- extensions/scarb-mdbook/src/main.rs
+++ extensions/scarb-mdbook/src/main.rs
@@ -44,7 +44,10 @@ fn main() -> ExitCode {
 fn main_inner(args: &Args, _ui: Ui) -> Result<()> {
     let mut book = MDBook::load(args.input.clone())?;
     let output_path: PathBuf = args.output.clone().into();
-    book.config.build.build_dir = output_path.strip_prefix(&args.input)?.into();
+    book.config.build.build_dir = output_path
+        .strip_prefix(&args.input)
+        .unwrap_or(&output_path.clone())
+        .into();
     book.build()?;
     let highlight = include_str!("../theme/highlight.js");
     let mut highlight_file = File::create(output_path.join("highlight.js"))?;
