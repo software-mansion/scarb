92c3b315e1a4cf6c5c37eaec1255c3ccbb4cd5a8
Maksim Zdobnikau <maksim.zdobnikau@swmansion.com>
2025-05-19 11:59:54 +0200

    filter out unsupported global args for all cmds

--- scarb/src/bin/scarb/commands/completions.rs
+++ scarb/src/bin/scarb/commands/completions.rs
@@ -3,6 +3,7 @@ use clap::{Arg, Command, CommandFactory};
 use clap_complete::{Shell as ClapShell, generate};
 use scarb::core::Config;
 use scarb::ops::{SubcommandDirs, list_external_subcommands};
+use std::collections::HashSet;
 use std::io;
 
 use crate::args::{CompletionsArgs, ScarbArgs};
@@ -25,11 +26,11 @@ pub fn run(args: CompletionsArgs, config: &Config) -> Result<()> {
 
 fn build_command(config: &Config) -> Result<Command> {
     let mut cmd = ScarbArgs::command();
-    let global_args = cmd
+    let global_args: HashSet<String> = ScarbArgs::command()
         .get_arguments()
         .filter(|arg| arg.is_global_set())
         .map(|arg| arg.get_id().to_string())
-        .collect::<Vec<_>>();
+        .collect();
 
     let dirs = SubcommandDirs::try_from(config).expect("Failed to get subcommand directories");
     let external_subcommands = list_external_subcommands(&dirs)?;
@@ -38,39 +39,45 @@ fn build_command(config: &Config) -> Result<Command> {
         let subcommand = if external_cmd.is_bundled {
             match external_cmd.name.as_str() {
                 cairo_language_server_args::COMMAND_NAME => {
-                    Some(cairo_language_server_args::Args::command())
+                    cairo_language_server_args::Args::command()
                 }
-                cairo_run_args::COMMAND_NAME => Some(cairo_run_args::Args::command()),
-                cairo_test_args::COMMAND_NAME => Some(cairo_test_args::Args::command()),
-                doc_args::COMMAND_NAME => Some(doc_args::Args::command()),
-                execute::COMMAND_NAME => Some(execute::Args::command()),
-                mdbook_args::COMMAND_NAME => Some(mdbook_args::Args::command()),
-                prove_args::COMMAND_NAME => Some(prove_args::Args::command()),
-                verify_args::COMMAND_NAME => Some(verify_args::Args::command()),
-                "test-support" => None,
-                _ => Some(
-                    Command::new(&external_cmd.name)
-                        .name(&external_cmd.name)
-                        .about(format!("Bundled '{}' extension", external_cmd.name))
-                        .disable_help_flag(true)
-                        .disable_version_flag(true)
-                        .args(global_args.iter().map(|name| Arg::new(name).hide(true))),
-                ),
+                cairo_run_args::COMMAND_NAME => cairo_run_args::Args::command(),
+                cairo_test_args::COMMAND_NAME => cairo_test_args::Args::command(),
+                doc_args::COMMAND_NAME => doc_args::Args::command(),
+                execute::COMMAND_NAME => execute::Args::command(),
+                mdbook_args::COMMAND_NAME => mdbook_args::Args::command(),
+                prove_args::COMMAND_NAME => prove_args::Args::command(),
+                verify_args::COMMAND_NAME => verify_args::Args::command(),
+                "test-support" => {
+                    continue;
+                }
+                _ => Command::new(&external_cmd.name)
+                    .name(&external_cmd.name)
+                    .about(format!("Bundled '{}' extension", external_cmd.name))
+                    .disable_help_flag(true)
+                    .disable_version_flag(true),
             }
         } else {
-            Some(
-                Command::new(&external_cmd.name)
-                    .name(&external_cmd.name)
-                    .about(format!("External '{}' extension", external_cmd.name))
-                    .disable_help_flag(true)
-                    .disable_version_flag(true)
-                    .hide_possible_values(true)
-                    .args(global_args.iter().map(|name| Arg::new(name).hide(true))),
-            )
+            Command::new(&external_cmd.name)
+                .name(&external_cmd.name)
+                .about(format!("External '{}' extension", external_cmd.name))
+                .disable_help_flag(true)
+                .disable_version_flag(true)
         };
-        if let Some(subcommand) = subcommand {
-            cmd = cmd.subcommand(subcommand);
-        }
+        let subcommand = sanitize_subcommand_args(subcommand, &global_args);
+        cmd = cmd.subcommand(subcommand);
     }
     Ok(cmd)
+}
+
+/// Hide unsupported global args from the subcommand completions by overriding them with local args and marking them as hidden.
+fn sanitize_subcommand_args(mut cmd: Command, global_args: &HashSet<String>) -> Command {
+    let local_args: HashSet<String> = cmd
+        .get_arguments()
+        .map(|arg| arg.get_id().to_string())
+        .collect();
+    for name in global_args.difference(&local_args) {
+        cmd = cmd.arg(Arg::new(name).hide(true));
+    }
+    cmd
 }
