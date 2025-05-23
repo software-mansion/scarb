0bbe54738dddff8bd3fa0a40b881d7329baa5c36
FroyaTheHen <kingacepielik@gmail.com>
2025-05-23 11:16:21 +0200

    feat: aggregate_groups - doc
    commit-id:a1c86ef7

---
+++ extensions/scarb-doc/src/types/groups.rs
@@ -0,0 +1,76 @@
+use crate::types::module_type::Module;
+use crate::types::other_types::{
+    Constant, Enum, ExternFunction, ExternType, FreeFunction, Impl, ImplAlias, Struct, Trait,
+    TypeAlias,
+};
+use std::collections::HashMap;
+
+#[derive(Clone)]
+pub struct Group {
+    pub name: String,
+    pub submodules: Vec<Module>,
+    pub constants: Vec<Constant>,
+    pub free_functions: Vec<FreeFunction>,
+    pub structs: Vec<Struct>,
+    pub enums: Vec<Enum>,
+    pub type_aliases: Vec<TypeAlias>,
+    pub impl_aliases: Vec<ImplAlias>,
+    pub traits: Vec<Trait>,
+    pub impls: Vec<Impl>,
+    pub extern_types: Vec<ExternType>,
+    pub extern_functions: Vec<ExternFunction>,
+}
+
+macro_rules! aggregate_groups {
+    ($fn_name:ident, $items_type:ty, $group_field:ident) => {
+        pub fn $fn_name(items: &mut $items_type, groups_map: &mut HashMap<String, Group>) {
+            let mut remaining_items = Vec::new();
+
+            for item in items.iter_mut() {
+                if let Some(group_name) = item.item_data.group.clone() {
+                    let group = groups_map
+                        .entry(group_name.clone())
+                        .or_insert_with(|| Group {
+                            name: group_name.clone(),
+                            submodules: vec![],
+                            constants: vec![],
+                            free_functions: vec![],
+                            structs: vec![],
+                            enums: vec![],
+                            type_aliases: vec![],
+                            impl_aliases: vec![],
+                            traits: vec![],
+                            impls: vec![],
+                            extern_types: vec![],
+                            extern_functions: vec![],
+                        });
+
+                    group.$group_field.push(item.clone());
+                } else {
+                    remaining_items.push(item.clone());
+                }
+            }
+            *items = remaining_items;
+        }
+    };
+}
+
+aggregate_groups!(aggregate_constants_groups, Vec<Constant>, constants);
+aggregate_groups!(
+    aggregate_free_functions_by_group,
+    Vec<FreeFunction>,
+    free_functions
+);
+aggregate_groups!(aggregate_structs_groups, Vec<Struct>, structs);
+aggregate_groups!(aggregate_enums_groups, Vec<Enum>, enums);
+aggregate_groups!(aggregate_type_aliases_groups, Vec<TypeAlias>, type_aliases);
+aggregate_groups!(aggregate_impl_aliases_groups, Vec<ImplAlias>, impl_aliases);
+aggregate_groups!(aggregate_traits_groups, Vec<Trait>, traits);
+aggregate_groups!(aggregate_impls_groups, Vec<Impl>, impls);
+aggregate_groups!(aggregate_extern_types_groups, Vec<ExternType>, extern_types);
+aggregate_groups!(
+    aggregate_extern_functions_by_group,
+    Vec<ExternFunction>,
+    extern_functions
+);
+aggregate_groups!(aggregate_modules_by_group, Vec<Module>, submodules);
