67ed8ee3b1d190904d7cf8a2f583d11c8dfdd9d0
Maciej Trątnowiecki <maciej.tratnowiecki@swmansion.com>
2025-06-12 22:14:15 +0200

    Bump cairo-lang-parser and syntax deps in tests

--- utils/scarb-test-support/src/cairo_plugin_project_builder.rs
+++ utils/scarb-test-support/src/cairo_plugin_project_builder.rs
@@ -128,11 +128,11 @@ impl CairoPluginProjectBuilder {
     }
 
     pub fn add_cairo_lang_parser_dep(self) -> Self {
-        self.add_dep(r#"cairo-lang-parser = { git = "https://github.com/starkware-libs/cairo.git", rev = "df9b4ea9a8aa0467ff98b7e70a447209c9c7ee89" }"#)
+        self.add_dep(r#"cairo-lang-parser = { git = "https://github.com/starkware-libs/cairo.git", rev = "172195a5e8c889a2355c28f5c444e61da6ae8a2b" }"#)
     }
 
     pub fn add_cairo_lang_syntax_dep(self) -> Self {
-        self.add_dep(r#"cairo-lang-syntax = { git = "https://github.com/starkware-libs/cairo.git", rev = "df9b4ea9a8aa0467ff98b7e70a447209c9c7ee89" }"#)
+        self.add_dep(r#"cairo-lang-syntax = { git = "https://github.com/starkware-libs/cairo.git", rev = "172195a5e8c889a2355c28f5c444e61da6ae8a2b" }"#)
     }
 
     pub fn default_v1() -> Self {
