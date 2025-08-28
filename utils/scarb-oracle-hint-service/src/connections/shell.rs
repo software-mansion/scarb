5210bde8c2819c23e584aa8aa611c02b88d63222
Marek Kaput <marek.kaput@swmansion.com>
2025-08-28 13:45:15 +0200

    Implement `shell` oracle protocol
    fix #2405

---
+++ utils/scarb-oracle-hint-service/src/connections/shell.rs
@@ -0,0 +1,157 @@
+use crate::Protocol;
+use crate::connection::Connection;
+use anyhow::{Context, Result, anyhow, ensure};
+use deno_task_shell::parser::{SequentialList, parse};
+use deno_task_shell::{ShellPipeReader, ShellState, pipe};
+use starknet_core::codec::{Decode, Encode};
+use starknet_core::types::{ByteArray, Felt};
+use std::env;
+use std::io::{BufRead, BufReader, Read};
+use std::sync::Arc;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use tokio::task::spawn_blocking;
+use tracing::{Span, debug, debug_span, warn};
+
+/// The `shell` protocol executes a provided shell invocation line and returns its stdout as
+/// a `ByteArray`. Stderr is redirected to the logs.
+pub struct Shell;
+
+impl Protocol for Shell {
+    const SCHEME: &'static str = "shell";
+
+    #[tracing::instrument]
+    fn connect(_command: &str) -> Result<Box<dyn Connection + 'static>> {
+        Ok(Box::new(ShellConnection::new()?))
+    }
+}
+
+struct ShellConnection {
+    rt: tokio::runtime::Runtime,
+}
+
+impl ShellConnection {
+    fn new() -> Result<Self> {
+        Ok(Self {
+            rt: tokio::runtime::Builder::new_current_thread().build()?,
+        })
+    }
+}
+
+impl Connection for ShellConnection {
+    fn call(&mut self, selector: &str, calldata: &[Felt]) -> Result<Vec<Felt>> {
+        // This oracle has several modes which are determined by suffixes of the selector.
+        // All selectors accept the same input format.
+        let mode = Mode::get(selector)?;
+
+        let span = Arc::new(debug_span!("exec", id = next_id()));
+        let _enter = span.enter();
+
+        // Decode arguments.
+        let mut calldata = calldata.iter();
+        let command: String = ByteArray::decode_iter(&mut calldata)?.try_into()?;
+        debug!("{command}");
+
+        // Run command.
+        let command = parse(&command)?;
+        let shell = build_shell()?;
+        let (exit_code, stdout) = self.rt.block_on(execute(span.clone(), command, shell));
+
+        // Finalise, according to mode.
+        mode.finalise(exit_code, stdout.into())
+    }
+}
+
+#[derive(Eq, PartialEq)]
+enum Mode {
+    /// `eo`: return (exit code, stdout) tuple.
+    ReturnExitReturnOutput,
+    /// `co`: require successful exit code, return stdout.
+    CheckExitReturnOutput,
+}
+
+impl Mode {
+    fn get(selector: &str) -> Result<Self> {
+        Ok(match selector {
+            "taskeo" => Self::ReturnExitReturnOutput,
+            "taskco" => Self::CheckExitReturnOutput,
+            _ => return Err(anyhow!("unsupported selector: {selector}")),
+        })
+    }
+
+    fn finalise(self, exit_code: i32, stdout: ByteArray) -> Result<Vec<Felt>> {
+        // Because Encode is not implemented for tuples in starknet-core...
+        macro_rules! encode_tuple {
+            ($($item:expr),* $(,)?) => {{
+                let mut felts = Vec::new();
+                $($item.encode(&mut felts)?;)*
+                felts
+            }};
+        }
+
+        match self {
+            Self::ReturnExitReturnOutput => Ok(encode_tuple!(Felt::from(exit_code), stdout)),
+
+            Self::CheckExitReturnOutput => {
+                ensure!(exit_code == 0, "command failed with exit code: {exit_code}");
+                Ok(encode_tuple!(stdout))
+            }
+        }
+    }
+}
+
+fn build_shell() -> Result<ShellState> {
+    Ok(ShellState::new(
+        env::vars_os().collect(),
+        env::current_dir().context("cannot get current dir")?,
+        Default::default(),
+        Default::default(),
+    ))
+}
+
+async fn execute(span: Arc<Span>, command: SequentialList, shell: ShellState) -> (i32, Vec<u8>) {
+    // Capture stdout.
+    let (stdout_reader, stdout_writer) = pipe();
+    let stdout_handle = spawn_blocking(move || {
+        let mut buf = Vec::new();
+        stdout_reader.pipe_to(&mut buf).unwrap();
+        buf
+    });
+
+    // Redirect stderr to logs.
+    let (ShellPipeReader::OsPipe(mut stderr_reader), stderr_writer) = pipe() else {
+        unreachable!();
+    };
+    let stderr_handle = spawn_blocking(move || {
+        let span = debug_span!(parent: &*span, "err");
+        pipe_to_tracing(&span, &mut stderr_reader);
+    });
+
+    let exit_code = deno_task_shell::execute_with_pipes(
+        command,
+        shell,
+        ShellPipeReader::stdin(),
+        stdout_writer,
+        stderr_writer,
+    )
+    .await;
+
+    let stdout = stdout_handle.await.unwrap();
+    stderr_handle.await.unwrap();
+    (exit_code, stdout)
+}
+
+fn pipe_to_tracing(span: &Span, stream: &mut dyn Read) {
+    let _enter = span.enter();
+    let stream = BufReader::with_capacity(128, stream);
+    for line in stream.lines() {
+        match line {
+            Ok(line) => debug!("{line}"),
+            Err(err) => warn!("{err:?}"),
+        }
+    }
+}
+
+fn next_id() -> usize {
+    static COUNTER: AtomicUsize = AtomicUsize::new(0);
+    COUNTER.fetch_add(1, Ordering::Relaxed)
+}
