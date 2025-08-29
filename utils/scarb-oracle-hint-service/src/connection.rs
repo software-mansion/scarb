e6fafa48bfd671316b25c65d66b15e7368341037
Marek Kaput <marek.kaput@swmansion.com>
2025-08-29 12:43:12 +0200

    Allow providing protocols externally (#2579)
    Allow providing protocols externally
    
    Also:
    - Renamed `ConnectionManager` to `Connections`, for brevity.
    - This enables turning some protocols on/off internally as everything is
    now controlled by one-liners in `OracleHintService::new`.
    
    Extract builtin protocol registration logic into a dedicated function

--- utils/scarb-oracle-hint-service/src/connection.rs
+++ utils/scarb-oracle-hint-service/src/connection.rs
@@ -1,5 +1,5 @@
-use crate::connections::stdio_jsonrpc::StdioJsonRpcConnection;
-use anyhow::{Result, bail};
+use crate::protocol::Protocols;
+use anyhow::Result;
 use starknet_core::types::Felt;
 use std::collections::HashMap;
 use std::collections::hash_map::Entry;
@@ -11,43 +11,32 @@ pub trait Connection {
 
 /// Maintains a collection of oracle [`Connection`]s.
 #[derive(Default)]
-pub struct ConnectionManager(HashMap<String, Box<dyn Connection + 'static>>);
+pub struct Connections(HashMap<String, Box<dyn Connection + 'static>>);
 
-impl ConnectionManager {
-    pub fn new() -> Self {
-        Self::default()
-    }
-
+impl Connections {
     /// Establishes a connection to a given connection string and stores it in the connection pool.
     ///
     /// If the connection already exists in the pool, the existing connection is reused.
     /// Erroneous connections aren't cached, and further connections will attempt to reconnect.
-    pub fn connect(&mut self, connection_string: &str) -> Result<&mut (dyn Connection + 'static)> {
+    pub fn connect(
+        &mut self,
+        connection_string: &str,
+        protocols: &Protocols,
+    ) -> Result<&mut (dyn Connection + 'static)> {
         match self.0.entry(connection_string.into()) {
             Entry::Occupied(entry) => Ok(entry.into_mut().as_mut()),
-            Entry::Vacant(entry) => Ok(entry
-                .insert(Self::create_connection(connection_string)?)
-                .as_mut()),
-        }
-    }
-
-    fn create_connection(connection_string: &str) -> Result<Box<dyn Connection + 'static>> {
-        if let Some(command) = connection_string.strip_prefix("stdio:") {
-            Ok(Box::new(StdioJsonRpcConnection::connect(command)?))
-        } else {
-            bail!(
-                "unsupported connection scheme: {connection_string:?}\n\
-                note: supported schemes are: `stdio`"
-            )
+            Entry::Vacant(entry) => {
+                Ok(entry.insert(protocols.connect(connection_string)?).as_mut())
+            }
         }
     }
 }
 
-impl fmt::Debug for ConnectionManager {
+impl fmt::Debug for Connections {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let mut connection_strings = self.0.keys().collect::<Vec<_>>();
         connection_strings.sort();
-        f.debug_tuple("ConnectionManager")
+        f.debug_tuple("Connections")
             .field(&connection_strings)
             .finish()
     }
