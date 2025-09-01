58d6b68c30408b0cbcb58953f15808cd47b62a0b
Maksim Zdobnikau <maksim.zdobnikau@swmansion.com>
2025-09-01 10:06:26 +0200

    fix tests

--- scarb/tests/audits.rs
+++ scarb/tests/audits.rs
@@ -125,7 +125,7 @@ fn require_audits_disallows_git_dep() {
         .lib_cairo("fn world() -> felt252 { foo::hello() }")
         .manifest_extra(
             r#"
-            [security]
+            [workspace]
             require-audits = true
         "#,
         )
@@ -142,9 +142,9 @@ fn require_audits_disallows_git_dep() {
             Caused by:
                 0: dependency `foo` from `git` source is not allowed when audit requirement is enabled
                 1: help: depend on a registry package
-                   alternatively, consider whitelisting dependency in package manifest
+                   alternatively, consider whitelisting dependency in workspace root manifest
                     --> Scarb.toml
-                       [security]
+                       [workspace]
                        allow-no-audits = ["foo"]
         "#});
 }
@@ -167,7 +167,7 @@ fn require_audits_disallows_path_dep() {
         .lib_cairo(r#"fn hello() -> felt252 { 0 }"#)
         .manifest_extra(
             r#"
-            [security]
+            [workspace]
             require-audits = true
         "#,
         )
@@ -184,9 +184,9 @@ fn require_audits_disallows_path_dep() {
             Caused by:
                 0: dependency `foo` from `path` source is not allowed when audit requirement is enabled
                 1: help: depend on a registry package
-                   alternatively, consider whitelisting dependency in package manifest
+                   alternatively, consider whitelisting dependency in workspace root manifest
                     --> Scarb.toml
-                       [security]
+                       [workspace]
                        allow-no-audits = ["foo"]
         "#});
 }
