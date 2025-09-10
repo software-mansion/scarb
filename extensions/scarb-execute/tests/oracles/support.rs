91e9a92d8c6897ebbd8ed45c7996cd288c2d1e7f
Marek Kaput <marek.kaput@swmansion.com>
2025-09-10 13:37:47 +0200

    Make CheckBuilder owned and add `src_binary` method

--- extensions/scarb-execute/tests/oracles/support.rs
+++ extensions/scarb-execute/tests/oracles/support.rs
@@ -1,10 +1,12 @@
 use assert_fs::TempDir;
+use camino::Utf8PathBuf;
 use derive_builder::Builder;
 use indoc::indoc;
 use scarb_test_support::command::Scarb;
 use scarb_test_support::project_builder::ProjectBuilder;
 
 #[derive(Builder)]
+#[builder(pattern = "owned")]
 pub struct Check {
     #[builder(setter(into))]
     lib_cairo: String,
@@ -19,20 +21,32 @@ pub struct Check {
 
     #[builder(default, setter(custom))]
     profile: Option<String>,
+
+    #[builder(default, setter(custom))]
+    pb_ops: Vec<Box<dyn FnOnce(ProjectBuilder) -> ProjectBuilder>>,
 }
 
 impl CheckBuilder {
-    pub fn check(&mut self) {
+    pub fn check(self) {
         self.build().unwrap().check();
     }
 
-    pub fn failure(&mut self) -> &mut Self {
+    pub fn failure(mut self) -> Self {
         self.failure = Some(true);
         self
     }
 
-    pub fn profile(&mut self, profile: String) -> &mut Self {
+    pub fn profile(mut self, profile: String) -> Self {
         self.profile = Some(Some(profile));
+        self
+    }
+
+    pub fn src_binary(mut self, path: impl Into<Utf8PathBuf>, content: impl Into<Vec<u8>>) -> Self {
+        let path = path.into();
+        let content = content.into();
+        self.pb_ops
+            .get_or_insert_default()
+            .push(Box::new(move |t| t.src_binary(path, content)));
         self
     }
 }
@@ -40,7 +54,7 @@ impl CheckBuilder {
 impl Check {
     pub fn check(self) {
         let t = TempDir::new().unwrap();
-        ProjectBuilder::start()
+        let mut pb = ProjectBuilder::start()
             .name("oracle_test")
             .version("0.1.0")
             .manifest_extra(indoc! {r#"
@@ -54,8 +68,13 @@ impl Check {
             .dep_starknet()
             .dep_oracle_asserts()
             .lib_cairo(self.lib_cairo)
-            .cp_test_oracle("test_oracle.py")
-            .build(&t);
+            .cp_test_oracle("test_oracle.py");
+
+        for op in self.pb_ops {
+            pb = op(pb);
+        }
+
+        pb.build(&t);
 
         let mut snapbox = Scarb::quick_snapbox().env("RUST_BACKTRACE", "0");
 
