087d55f581c52790e1dd82ba5681ee4f6a01f706
Marek Kaput <marek.kaput@swmansion.com>
2025-09-23 12:51:47 +0200

    Simplify shell oracle capabilities (#2661)
    Replaced `task**` selectors with just one `exec`. The checking behaviour
    can be easily done on the Cairo side. Also added a test for nonexistent
    command calls.

--- extensions/scarb-execute/tests/oracles/shell.rs
+++ extensions/scarb-execute/tests/oracles/shell.rs
@@ -1,7 +1,7 @@
 use crate::support::CheckBuilder;
 use indoc::formatdoc;
 
-fn check(selector: &str, command: &str, output_type: &str, expected_output: &str) {
+fn check(command: &str, expected_output: &str) {
     CheckBuilder::default()
         .lib_cairo(formatdoc! {r#"
             #[executable]
@@ -9,11 +9,11 @@ fn check(selector: &str, command: &str, output_type: &str, expected_output: &str
                 let mut inputs: Array<felt252> = array![];
                 let connection_string: ByteArray = "shell:";
                 connection_string.serialize(ref inputs);
-                '{selector}'.serialize(ref inputs);
+                'exec'.serialize(ref inputs);
                 let command: ByteArray = "{command}";
                 command.serialize(ref inputs);
                 let result = starknet::testing::cheatcode::<'oracle_invoke'>(inputs.span());
-                oracle_asserts::print::<{output_type}>(result);
+                oracle_asserts::print::<(i32, ByteArray)>(result);
             }}
         "#})
         .stdout_matches(formatdoc! {r#"
@@ -27,41 +27,16 @@ fn check(selector: &str, command: &str, output_type: &str, expected_output: &str
 }
 
 #[test]
-fn taskeo_success() {
-    check(
-        "taskeo",
-        "echo hello",
-        "(i32, ByteArray)",
-        "Result::Ok((0, \"hello\n\"))",
-    )
+fn exec_success() {
+    check("echo hello", "Result::Ok((0, \"hello\n\"))")
 }
 
 #[test]
-fn taskeo_failure() {
-    check(
-        "taskeo",
-        "exit 1",
-        "(i32, ByteArray)",
-        "Result::Ok((1, \"\"))",
-    )
+fn exec_failure() {
+    check("exit 1", "Result::Ok((1, \"\"))")
 }
 
 #[test]
-fn taskco_success() {
-    check(
-        "taskco",
-        "echo hello",
-        "ByteArray",
-        "Result::Ok(\"hello\n\")",
-    )
-}
-
-#[test]
-fn taskco_failure() {
-    check(
-        "taskco",
-        "exit 1",
-        "ByteArray",
-        "Result::Err(\"command failed with exit code: 1\")",
-    )
+fn exec_nonexistent_command() {
+    check("nonexistent_command_123", "Result::Ok((127, \"\"))")
 }
