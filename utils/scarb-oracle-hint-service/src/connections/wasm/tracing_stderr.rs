b8b78a72c5572cf9b2d378bd0a13ea015c3c65c9
Marek Kaput <marek.kaput@swmansion.com>
2025-09-24 14:44:42 +0200

    Q&A

--- utils/scarb-oracle-hint-service/src/connections/wasm/tracing_stderr.rs
+++ utils/scarb-oracle-hint-service/src/connections/wasm/tracing_stderr.rs
@@ -32,6 +32,22 @@ impl StdoutStream for TracingStderrWriter {
 }
 
 impl AsyncWrite for TracingStderrWriter {
+    // NOTE: This is AsyncWrite only because wasmtime takes such an interface for piping stdio.
+    //   Our implementation is entirely blocking, we dump the output as it comes directly to logs
+    //   and wait for them to be handled by subscribers. There are two questions one might ask:
+    //
+    //   Q: Could synchronous stderr logging slow down WASM execution?
+    //   A: Theoretically, yes. Synchronous emission means the WASM host thread waits for logging.
+    //      This is intentional to preserve execution-log correlation and immediate log visibility.
+    //      But WASM is executed here as a sidecar to a zero-knowledge proving runtime. It is not
+    //      expected for these programs to be realtime or emit large amounts of logs.
+    //
+    //   Q: Is chopping buffers by lines and treating trailing data as full line correct?
+    //      Won't this cause some log lines to be split in half?
+    //   A: Most runtimes (and primarily Rust, which matters the most for us) buffer stdio by full
+    //      lines, so this naturally plays well. For others... does it actually matter? This is
+    //      just a log stream, and thus information will be still transmitted fully in weird format.
+
     fn poll_write(
         self: Pin<&mut Self>,
         _cx: &mut Context<'_>,
