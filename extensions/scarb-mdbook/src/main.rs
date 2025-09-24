58cdec28ee5c7ae1c9f5b4a4ea11e821a8a8c0af
Kinga Cepielik <51486024+FroyaTheHen@users.noreply.github.com>
2025-09-24 09:59:12 +0200

    Propagate mdbook stderr in scarb doc (#2650)
    closes https://github.com/software-mansion/scarb/issues/2607

--- extensions/scarb-mdbook/src/main.rs
+++ extensions/scarb-mdbook/src/main.rs
@@ -6,6 +6,7 @@ use clap::Parser;
 use mdbook::MDBook;
 use scarb_extensions_cli::mdbook::Args;
 use scarb_ui::Ui;
+use scarb_ui::args::VerbositySpec;
 use std::fs::File;
 use std::io::Write;
 use std::path::PathBuf;
@@ -14,6 +15,7 @@ use std::process::ExitCode;
 fn main() -> ExitCode {
     let args = Args::parse();
     let ui = Ui::new(args.verbose.clone().into(), scarb_ui::OutputFormat::Text);
+    init_logging(args.verbose.clone(), &ui);
     let build_result = main_inner(&args, ui.clone()).with_context(|| {
         format!(
             "failed to build book from `{}` source path",
@@ -41,4 +43,18 @@ fn main_inner(args: &Args, _ui: Ui) -> Result<()> {
     let mut highlight_file = File::create(output_path.join("highlight.js"))?;
     highlight_file.write_all(highlight.as_bytes())?;
     Ok(())
+}
+
+fn init_logging(verbose: VerbositySpec, ui: &Ui) {
+    use tracing_subscriber::filter::EnvFilter;
+    use tracing_subscriber::fmt::time::Uptime;
+
+    tracing_subscriber::fmt()
+        .with_writer(std::io::stderr)
+        .with_ansi(ui.has_colors_enabled_stderr())
+        .with_timer(Uptime::default())
+        .with_env_filter(EnvFilter::new(
+            verbose.as_trace().replace("scarb", "mdbook"),
+        ))
+        .init();
 }
