d7c8addf5d0c8f201adcfe3f60e58fdeee18a15e
Marek Kaput <marek.kaput@swmansion.com>
2025-10-03 11:32:40 +0200

    Add test to ensure dependency build scripts are ignored during build

--- scarb/tests/build_script.rs
+++ scarb/tests/build_script.rs
@@ -1,4 +1,5 @@
 use assert_fs::TempDir;
+use assert_fs::prelude::*;
 use indoc::indoc;
 
 use scarb_test_support::command::Scarb;
@@ -24,6 +25,36 @@ fn build_script_runs_before_compilation() {
             [..]Running build script for hello v1.0.0 ([..]Scarb.toml)
             Prebuild script executed
             [..]Compiling hello v1.0.0 ([..]Scarb.toml)
+            [..]Finished `dev` profile target(s) in [..]
+        "#});
+}
+
+#[test]
+fn ignore_dependency_build_script() {
+    let t = TempDir::new().unwrap();
+
+    let dep = t.child("dep");
+    ProjectBuilder::start()
+        .name("dep")
+        .manifest_extra(indoc! {r#"
+            [scripts]
+            build = "echo 'THIS SHOULD NOT BE PRINTED'"
+        "#})
+        .build(&dep);
+
+    let main = t.child("main");
+    ProjectBuilder::start()
+        .name("main")
+        .dep("dep", &dep)
+        .build(&main);
+
+    Scarb::quick_snapbox()
+        .arg("build")
+        .current_dir(&main)
+        .assert()
+        .success()
+        .stdout_matches(indoc! {r#"
+            [..]Compiling main v1.0.0 ([..]Scarb.toml)
             [..]Finished `dev` profile target(s) in [..]
         "#});
 }
