e1cc3397bc9974fe739259d2f5cc5fb518304bfb
maciektr <maciej.tratnowiecki@swmansion.com>
2025-11-13 15:31:55 +0100

    Update deps (#2837)
    - **Bump the non-critical group across 1 directory with 11 updates**
    - **Update Cargo.toml**
    - **Remove use of deprecated snapbox::cargo_bin**
    - **Update Cargo.toml**
    - **Copy deprecated function from snapbox**
    - **Update lock**
    - **Pass std path to Library::new**
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Signed-off-by: maciektr <maciej.tratnowiecki@swmansion.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- utils/scarb-test-support/src/cargo.rs
+++ utils/scarb-test-support/src/cargo.rs
@@ -6,7 +6,31 @@ pub fn cargo_bin(name: &str) -> PathBuf {
     env::var_os(format!("NEXTEST_BIN_EXE_{name}"))
         .or_else(|| env::var_os(format!("CARGO_BIN_EXE_{name}")))
         .map(PathBuf::from)
-        .unwrap_or_else(|| snapbox::cmd::cargo_bin(name))
+        .unwrap_or_else(|| lookup_cargo_bin(name))
+}
+
+/// Look up the path to a cargo-built binary within an integration test.
+///
+/// Makes assumptions about Cargo.
+fn lookup_cargo_bin(name: &str) -> PathBuf {
+    let file_name = format!("{}{}", name, std::env::consts::EXE_SUFFIX);
+    let target_dir = target_dir();
+    target_dir.join(file_name)
+}
+
+// Adapted from
+// https://github.com/rust-lang/cargo/blob/485670b3983b52289a2f353d589c57fae2f60f82/tests/testsuite/support/mod.rs#L507
+fn target_dir() -> PathBuf {
+    env::current_exe()
+        .ok()
+        .map(|mut path| {
+            path.pop();
+            if path.ends_with("deps") {
+                path.pop();
+            }
+            path
+        })
+        .unwrap()
 }
 
 pub fn manifest_dir() -> PathBuf {
