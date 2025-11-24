83fb063cff867fab4330f8ad95ad2a3a167ed598
Maksim Zdobnikau <maksim.zdobnikau@swmansion.com>
2025-11-24 04:02:44 +0300

    add common `incremental_create_execution_output_dir`

--- utils/create-output-dir/src/lib.rs
+++ utils/create-output-dir/src/lib.rs
@@ -6,10 +6,13 @@
 //!
 //! [cargo-util-fn]: https://docs.rs/cargo-util/latest/cargo_util/paths/fn.create_dir_all_excluded_from_backups_atomic.html
 
-use anyhow::{Context, Result};
+use anyhow::{bail, Context, Result};
 use std::ffi::OsStr;
 use std::path::Path;
-use std::{env, fs};
+use std::{env, fs, io};
+use camino::{Utf8Path, Utf8PathBuf};
+
+const MAX_ITERATION_COUNT: usize = 10000;
 
 pub const EXECUTE_PROGRAM_OUTPUT_FILENAME: &str = "program_output.txt";
 pub const EXECUTE_PRINT_OUTPUT_FILENAME: &str = "stdout_output.txt";
@@ -58,6 +61,27 @@ pub fn create_output_dir(path: &Path) -> Result<()> {
     }
 
     Ok(())
+}
+
+/// Creates an incremental execution output dir inside the given path.
+/// The dir name is `execution{N}` with the lowest `N` without existing dir.
+///
+/// Returns the path to the created directory and corresponding `N`.
+pub fn incremental_create_execution_output_dir(path: &Utf8Path) -> Result<(Utf8PathBuf, usize)> {
+    for i in 1..=MAX_ITERATION_COUNT {
+        let filepath = path.join(format!("execution{i}"));
+        let result = fs::create_dir(&filepath);
+        return match result {
+            Err(e) => {
+                if e.kind() == io::ErrorKind::AlreadyExists {
+                    continue;
+                }
+                Err(e.into())
+            }
+            Ok(_) => Ok((filepath, i)),
+        };
+    }
+    bail!("failed to create output directory")
 }
 
 /// Marks the directory as excluded from archives/backups.
