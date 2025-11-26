4698da4a8f0f57b65cda322f3fb2115cc1416d40
Maksim Zdobnikau <maksim.zdobnikau@swmansion.com>
2025-11-26 20:55:55 +0300

    more renames

--- extensions/scarb-doc/tests/runnable_examples.rs
+++ extensions/scarb-doc/tests/runnable_examples.rs
@@ -6,15 +6,15 @@ use scarb_test_support::project_builder::ProjectBuilder;
 mod markdown_target;
 use markdown_target::MarkdownTargetChecker;
 
-const CODE_WITH_SNIPPETS: &str = include_str!("code/code_12.cairo");
-const EXPECTED_CODE_WITH_SNIPPETS_PATH: &str = "tests/data/runnable_snippets";
+const CODE_WITH_RUNNABLE_CODE_BLOCKS: &str = include_str!("code/code_12.cairo");
+const EXPECTED_WITH_EMBEDDINGS_PATH: &str = "tests/data/runnable_examples";
 
 #[test]
 fn supports_runnable_examples() {
     let t = TempDir::new().unwrap();
     ProjectBuilder::start()
         .name("hello_world")
-        .lib_cairo(CODE_WITH_SNIPPETS)
+        .lib_cairo(CODE_WITH_RUNNABLE_CODE_BLOCKS)
         .build(&t);
 
     Scarb::quick_command()
@@ -25,13 +25,13 @@ fn supports_runnable_examples() {
         .assert()
         .success()
         .stdout_eq(formatdoc! {r#"
-            [..] Executing snippet #0 from `hello_world::foo_bar`
-            [..] Compiling hello_world_snippet_0 v0.1.0 ([..])
+            [..] Executing example #0 from `hello_world::foo_bar`
+            [..] Compiling hello_world_example_0 v0.1.0 ([..])
             [..]  Finished `dev` profile target(s) in [..]
-            [..] Executing hello_world_snippet_0
+            [..] Executing hello_world_example_0
             foo
             bar
-            Saving output to: target/execute/hello_world_snippet_0/execution1
+            Saving output to: target/execute/hello_world_example_0/execution1
             Saving output to: target/doc/hello_world
             Saving build output to: target/doc/hello_world/book
 
@@ -44,6 +44,6 @@ fn supports_runnable_examples() {
 
     MarkdownTargetChecker::lenient()
         .actual(t.path().join("target/doc/hello_world").to_str().unwrap())
-        .expected(EXPECTED_CODE_WITH_SNIPPETS_PATH)
+        .expected(EXPECTED_WITH_EMBEDDINGS_PATH)
         .assert_all_files_match();
 }
