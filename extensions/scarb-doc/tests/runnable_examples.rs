46cf01290bd4dd7222d616875eba7f75bb74809f
Maciej Trątnowiecki <maciej.tratnowiecki@swmansion.com>
2026-02-05 14:37:39 +0100

    Add test cases for mdx and json outputs

--- extensions/scarb-doc/tests/runnable_examples.rs
+++ extensions/scarb-doc/tests/runnable_examples.rs
@@ -6,12 +6,16 @@ use scarb_test_support::project_builder::ProjectBuilder;
 mod markdown_target;
 use markdown_target::MarkdownTargetChecker;
 
+mod json_target;
+use json_target::JsonTargetChecker;
+
 const CODE_WITH_RUNNABLE_CODE_BLOCKS: &str = include_str!("code/code_12.cairo");
 const CODE_WITH_COMPILE_ERROR: &str = include_str!("code/code_13.cairo");
 const CODE_WITH_RUNTIME_ERROR: &str = include_str!("code/code_14.cairo");
 const CODE_WITH_MULTIPLE_CODE_BLOCKS_PER_ITEM: &str = include_str!("code/code_15.cairo");
 const CODE_WITH_STARKNET_CONTRACT: &str = include_str!("code/code_16.cairo");
 const EXPECTED_WITH_EMBEDDINGS_PATH: &str = "tests/data/runnable_examples";
+const EXPECTED_WITH_EMBEDDINGS_MDX_PATH: &str = "tests/data/runnable_examples_mdx";
 const EXPECTED_MULTIPLE_PER_ITEM_PATH: &str = "tests/data/runnable_examples_multiple_per_item";
 
 #[test]
@@ -53,6 +57,71 @@ fn supports_runnable_examples() {
         .actual(t.path().join("target/doc/hello_world").to_str().unwrap())
         .expected(EXPECTED_WITH_EMBEDDINGS_PATH)
         .assert_all_files_match();
+}
+
+#[test]
+fn supports_runnable_examples_mdx() {
+    let t = TempDir::new().unwrap();
+    ProjectBuilder::start()
+        .name("hello_world")
+        .lib_cairo(CODE_WITH_RUNNABLE_CODE_BLOCKS)
+        .build(&t);
+
+    Scarb::quick_command()
+        .arg("doc")
+        .args(["--output-format", "mdx"])
+        .arg("--disable-remote-linking")
+        .current_dir(&t)
+        .assert()
+        .success()
+        .stdout_eq(formatdoc! {r#"
+            [..] Running 3 doc examples for `hello_world`
+            test hello_world::bar ... ignored
+            test hello_world::foo ... ignored
+            [..] Compiling hello_world_example_1 v0.1.0 ([..])
+            [..]  Finished `dev` profile target(s) in [..]
+            test hello_world::foo_bar ... ok
+
+            test result: ok. 1 passed; 0 failed; 2 ignored
+            Saving output to: target/doc/hello_world
+        "#});
+
+    MarkdownTargetChecker::lenient()
+        .actual(t.path().join("target/doc/hello_world").to_str().unwrap())
+        .expected(EXPECTED_WITH_EMBEDDINGS_MDX_PATH)
+        .assert_all_files_match();
+}
+
+#[test]
+fn supports_runnable_examples_json() {
+    let t = TempDir::new().unwrap();
+    ProjectBuilder::start()
+        .name("hello_world")
+        .lib_cairo(CODE_WITH_RUNNABLE_CODE_BLOCKS)
+        .build(&t);
+
+    Scarb::quick_command()
+        .arg("doc")
+        .args(["--output-format", "json"])
+        .current_dir(&t)
+        .assert()
+        .success()
+        .stdout_eq(formatdoc! {r#"
+            [..] Running 3 doc examples for `hello_world`
+            test hello_world::bar ... ignored
+            test hello_world::foo ... ignored
+            [..] Compiling hello_world_example_1 v0.1.0 ([..])
+            [..]  Finished `dev` profile target(s) in [..]
+            test hello_world::foo_bar ... ok
+
+            test result: ok. 1 passed; 0 failed; 2 ignored
+            Saving output to: target/doc/output.json
+        "#});
+
+    JsonTargetChecker::default()
+        .actual(&t.path().join("target/doc/output.json"))
+        .expected("./data/runnable_examples_json.json")
+        .assert_files_match();
 }
 
 #[test]
