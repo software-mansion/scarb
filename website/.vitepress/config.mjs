7a027928e41abb8a813cad19f4466a8fd4cc1c5a
Tomasz Rejowski <34059895+Arcticae@users.noreply.github.com>
2026-06-15 11:13:52 +0200

    Remove goatcounter setup (#3154)
    We don't use it anymore anyways
    This reverts commit 31bdbe4f9dbb927f9818ea395097b21fbe4590a5.

--- website/.vitepress/config.mjs
+++ website/.vitepress/config.mjs
@@ -189,14 +189,6 @@ export default withMermaid(
       ["meta", { property: "og:image:height", content: "640" }],
       [
         "script",
-        {
-          "data-goatcounter": "https://gc-scarb.swmtest.xyz/count",
-          async: true,
-          src: `${base}count.js`,
-        },
-      ],
-      [
-        "script",
         {},
         `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
